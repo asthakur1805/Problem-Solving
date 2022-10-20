b44da6fa2d6534a3a1ad5db6136f53fbe8296f61
Aditya Thakur <ast.thakur@gmail.com>
2022-10-20 19:39:59 -0400

    Time: 34 ms (95.57%), Space: 14.2 MB (35.71%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+
+	def subsets(self, nums):
+
+		index,currSubset,result, = 0,[],[]
+
+		self.helper(nums, index, currSubset, result)
+
+		return result
+
+	def helper(self, nums, index, currSubset, result):
+
+		if index == len(nums):
+
+			result.append(currSubset.copy())
+
+			return
+
+		currSubset.append(nums[index])
+
+		self.helper(nums, index+1, currSubset, result)
+
+		currSubset.pop()
+
+		self.helper(nums, index+1, currSubset, result)
