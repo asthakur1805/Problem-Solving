b6fcca161b3054af9d309e6df930d3f5d184c7a1
Aditya Thakur <ast.thakur@gmail.com>
2022-10-22 19:58:51 -0400

    Time: 81 ms (49.42%), Space: 14.3 MB (55.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -2,7 +2,39 @@ class Solution:
 
 	def search(self, nums, target):
 
+		if not nums:
+			return -1
+	
 		leftPointer, rightPointer = 0, len(nums)-1
+
+		while leftPointer <= rightPointer:
+
+			midPointer = leftPointer + (rightPointer - leftPointer) // 2
+
+			if leftPointer == rightPointer or nums[midPointer] > nums[midPointer+1]:
+
+				pivotIndex = midPointer
+
+				break
+
+			if nums[0] <= nums[midPointer]:
+
+				leftPointer = midPointer + 1
+
+			else:
+
+				rightPointer = midPointer - 1
+
+		if nums[0] <= target <= nums[pivotIndex]:
+
+			return self.binarySearch(nums, target, 0, pivotIndex)
+
+		return self.binarySearch(nums, target, pivotIndex+1, len(nums)-1)
+
+			
+
+		
+	def binarySearch(self, nums, target, leftPointer, rightPointer):
 
 		while leftPointer <= rightPointer:
 
@@ -12,24 +44,12 @@ class Solution:
 
 				return midPointer
 
-			if nums[leftPointer]  <= nums[midPointer]:
+			if target < nums[midPointer]:
 
-				if nums[leftPointer] <= target <= nums[midPointer]:
-
-					rightPointer = midPointer - 1
-
-				else:
-
-					leftPointer = midPointer + 1
+				rightPointer = midPointer - 1
 
 			else:
-
-				if nums[midPointer] <= target <= nums[rightPointer]:
-
-					leftPointer = midPointer + 1
-
-				else:
-
-					rightPointer = midPointer - 1
+		
+				leftPointer = midPointer + 1
 
 		return -1
