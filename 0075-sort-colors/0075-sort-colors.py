547fe35ef98ab7ba40e2f94b34fc396015f8c160
Aditya Thakur <ast.thakur@gmail.com>
2022-10-25 10:50:11 -0400

    Time: 53 ms (70.39%), Space: 13.9 MB (64.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,26 +2,31 @@ class Solution:
 
 	def sortColors(self, nums):
 
-		leftPointer = midPointer = 0
+		numCounts = {key:0 for key in [0,1,2]}
+
+		for num in nums:
+
+			numCounts[num] += 1
+
+		index = 0
+
+		for _ in range(numCounts[0]):
+
+			nums[index] = 0
+
+			index += 1
+
+		for _ in range(numCounts[1]):
+
+			nums[index] = 1
+
+			index += 1
+
+		for _ in range(numCounts[2]):
+
+			nums[index] = 2
+
+			index += 1
+
+
 		
-		rightPointer = len(nums)-1
-
-		while midPointer <= rightPointer:
-
-			if nums[midPointer] == 0:
-
-				nums[midPointer], nums[leftPointer] = nums[leftPointer], nums[midPointer]
-
-				midPointer += 1
-			
-				leftPointer += 1
-
-			elif nums[midPointer] == 1:
-
-				midPointer += 1
-
-			else:
-
-				nums[midPointer], nums[rightPointer] = nums[rightPointer], nums[midPointer]
-
-				rightPointer -= 1
