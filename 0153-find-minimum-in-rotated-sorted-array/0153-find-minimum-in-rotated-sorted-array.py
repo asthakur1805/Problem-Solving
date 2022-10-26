b1a2dce92cd01facb770a221189967eb36ccdc43
Aditya Thakur <ast.thakur@gmail.com>
2022-10-26 18:03:18 -0400

    Time: 46 ms (88.96%), Space: 14.2 MB (23.11%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -2,22 +2,18 @@ class Solution:
 
 	def findMin(self, nums):
 
-		if not nums:
-			return 
-
 		leftPointer, rightPointer = 0, len(nums)-1
 
 		while leftPointer <= rightPointer:
 
 			midPointer = leftPointer + (rightPointer - leftPointer) // 2
 
-			if leftPointer == rightPointer or nums[midPointer] > nums[midPointer+1]:
+			if leftPointer == rightPointer or nums[midPointer] > nums[midPointer + 1]:
 
 				pivotIndex = midPointer
-
 				break
 
-			if  nums[0] <= nums[midPointer]:
+			if nums[leftPointer] <= nums[midPointer]:
 
 				leftPointer = midPointer + 1
 
@@ -25,6 +21,7 @@ class Solution:
 
 				rightPointer = midPointer - 1
 
-		result = nums[0] if pivotIndex == len(nums)-1 else nums[pivotIndex+1]
+
+		result = nums[0] if pivotIndex == len(nums)-1 else nums[pivotIndex + 1]
 
 		return result
