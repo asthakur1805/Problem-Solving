1e98bad663ff84ec3b01ee5de3f48be0f0418574
Aditya Thakur <ast.thakur@gmail.com>
2022-11-03 16:13:36 -0400

    Time: 2454 ms (42.24%), Space: 13.9 MB (94.38%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -4,7 +4,7 @@ class Solution:
 
 		nums.sort()
 
-		result, builder = [], []
+		builder, result = [], []
 
 		self.helper(nums, target, 4, 0, builder, result)
 
@@ -12,54 +12,51 @@ class Solution:
 
 	def helper(self, nums, target, K, index, builder, result):
 
-		if K != 2:
+		if K == 2:
 
-			for currIndex in range(index, len(nums)-K+1):
+			leftPointer, rightPointer = index, len(nums)-1
 
-				if currIndex > index and nums[currIndex] == nums[currIndex-1]:
+			while leftPointer < rightPointer:
 
-					continue
+				addition = nums[leftPointer] + nums[rightPointer]
 
-				builder.append(nums[currIndex])
-
-				self.helper(nums, target - nums[currIndex], K-1, currIndex+1, builder, result)
-
-				builder.pop()
-
-			return
-
-		leftPointer, rightPointer = index, len(nums)-1
-
-		while leftPointer < rightPointer:
-
-			addition = nums[leftPointer] + nums[rightPointer]
-
-			if addition < target:
-
-				leftPointer += 1
-
-			elif addition > target:
-
-				rightPointer -= 1
-
-			else:
-
-				builder.append(nums[leftPointer])
-				builder.append(nums[rightPointer])
-
-				result.append(builder.copy())
-
-				for _ in range(2):
-					builder.pop()
-
-				leftPointer += 1
-
-				while leftPointer < rightPointer and nums[leftPointer]==nums[leftPointer-1]:
+				if addition < target:
 
 					leftPointer += 1
 
+				elif addition > target:
+
+					rightPointer -= 1
+
+				else:
+
+					builder.append(nums[leftPointer])
+					builder.append(nums[rightPointer])
+
+					result.append(builder.copy())
+
+					for _ in range(2):
+
+						builder.pop()
+
+					leftPointer += 1
+
+					while leftPointer < rightPointer and nums[leftPointer] == nums[leftPointer-1]:
+					
+						leftPointer += 1
+
+			return
+
+		for currIndex in range(index, len(nums)-(K-1)):
+
+			if currIndex > index and nums[currIndex] == nums[currIndex-1]:
+
+				continue
+
+			builder.append(nums[currIndex])
+
+			self.helper(nums, target-nums[currIndex], K-1, currIndex+1, builder, result)
+
+			builder.pop()
+
 		return
-
-				
-
-			
