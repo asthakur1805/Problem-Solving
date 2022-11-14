c61b905565919f2498355e7d87c4bac45039a607
Aditya Thakur <ast.thakur@gmail.com>
2022-11-14 00:34:59 -0500

    Time: 49 ms (95.51%), Space: 15.3 MB (92.28%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+
+	def connect(self, root):
+
+		if not root:
+			return
+
+		queue = collections.deque([root])
+
+		while queue:
+
+			levelCount = len(queue)
+
+			for nodeNumber in range(levelCount):
+
+				node = queue.popleft()
+
+				node.next = None if nodeNumber == levelCount-1 else queue[0]
+				
+				if node.left:
+					queue.append(node.left)
+				
+				if node.right:
+					queue.append(node.right)
+
+		return root
+		
