e6d2f6edd8c2ab17147f439e6f2a6149f50e8c4e
Aditya Thakur <ast.thakur@gmail.com>
2022-11-14 13:13:02 -0500

    Time: 97 ms (9.98%), Space: 14 MB (60.24%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,37 +1,25 @@
 class Solution:
-	
-	def permute(self, nums):
 
-		builder = []
+	def permute(self, nums):
 
 		result = []
 
-		numberMap = set()
-
-		self.helper(nums, builder, numberMap, result)
+		self.helper(nums, 0, result)
 
 		return result
 
-	def helper(self, nums, builder, numberMap, result):
+	def helper(self, nums, index, result):
 
-		if len(builder) == len(nums):
-
-			result.append(builder.copy())
-
-			return 
-
-		for num in nums:
-
-			if num not in numberMap:
-
-				builder.append(num)
-				numberMap.add(num)
-
-				self.helper(nums, builder, numberMap, result)
-
-				builder.pop()
-				numberMap.remove(num)
-
-		
-
+		if index == len(nums):
 			
+			result.append(nums.copy())
+			return
+
+		for swapIndex in range(index,len(nums)):
+
+			nums[index], nums[swapIndex] = nums[swapIndex], nums[index]
+
+			self.helper(nums, index+1, result)
+
+			nums[index], nums[swapIndex] = nums[swapIndex], nums[index]
+
