a9e8af6d9ca8669a1919a1e00782079aa6802945
Aditya Thakur <ast.thakur@gmail.com>
2022-11-14 17:58:24 -0500

    Time: 49 ms (99.71%), Space: 14 MB (99.36%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -8,22 +8,23 @@ class Solution:
 
 		result = []
 
-		numsUsed = [False]*len(nums)
+		numsUsed = [False] * len(nums)
 
 		self.helper(nums, builder, numsUsed, result)
 
 		return result
 
-
 	def helper(self, nums, builder, numsUsed, result):
 
 		if len(builder) == len(nums):
+			
 			result.append(builder.copy())
 			return
 
-		for index, num in enumerate(nums):
+		for index,num in enumerate(nums):
 
 			if numsUsed[index] or index > 0 and nums[index] == nums[index-1] and not numsUsed[index-1]:
+
 				continue
 
 			numsUsed[index] = True
