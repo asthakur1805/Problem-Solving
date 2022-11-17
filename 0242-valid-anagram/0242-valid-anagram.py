ee57407ef0cc259727fdf2a160abdd2bcb8e1609
Aditya Thakur <ast.thakur@gmail.com>
2022-11-17 13:03:07 -0500

    Time: 90 ms (61.44%), Space: 15.2 MB (11.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,21 +2,4 @@ class Solution:
 
 	def isAnagram(self, firstStr, secondStr):
 
-		if len(firstStr) != len(secondStr):
-			return False
-
-		firstStrMap, secondStrMap = {}, {}
-
-		for charIndex in range(len(firstStr)):
-
-			firstStrMap[firstStr[charIndex]] = 1 + firstStrMap.get(firstStr[charIndex], 0)
-
-			secondStrMap[secondStr[charIndex]] = 1 + secondStrMap.get(secondStr[charIndex], 0)
-
-		for character in firstStrMap:
-
-			if firstStrMap[character] != secondStrMap.get(character, 0):
-
-				return False
-
-		return True
+		return sorted(firstStr) == sorted(secondStr)
