750060fd55c8a5e9cded30b440d361862e33760f
Aditya Thakur <ast.thakur@gmail.com>
2022-11-22 12:32:09 -0500

    Time: 116 ms (24.82%), Space: 13.9 MB (35.39%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -2,24 +2,40 @@ class Solution:
 
 	def intToRoman(self, inputNumber):
 
-		conversion = [(1000,'M'), (900,'CM'), (500,'D'), (400,'CD'), (100,'C'), (90,'XC'), (50,'L'), (40,'XL'), (10,'X'), (9,'IX'), (5,'V'), (4,'IV'), (1,'I')]
+		conversion = [
+			(1000, 'M'),
+			(900, 'CM'),
+			(500, 'D'),
+			(400, 'CD'),
+			(100, 'C'),
+			(90, 'XC'),
+			(50, 'L'),
+			(40, 'XL'),
+			(10, 'X'),
+			(9, 'IX'),
+			(5, 'V'),
+			(4, 'IV'),
+			(1, 'I')
+		]
 
-		index = 0
+		romanIndex = 0
 
-		resultRoman = []
+		result = []
 
 		while inputNumber:
 
-			value, symbol = conversion[index]
+			baseNumber, roman = conversion[romanIndex]
 
-			if inputNumber >= value:
+			if inputNumber >= baseNumber:
 
-				repeat = inputNumber // value
+				repeat = inputNumber // baseNumber
 
-				resultRoman.append(symbol*repeat)
+				result.append(roman * repeat)
 
-				inputNumber %= value
+				inputNumber %= baseNumber
 
-			index += 1
+			romanIndex += 1
 
-		return ''.join(resultRoman)
+		return ''.join(result)
+
+				
