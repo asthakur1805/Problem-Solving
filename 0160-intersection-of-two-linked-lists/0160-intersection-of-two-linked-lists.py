28404795ecc17417dc87d3b04e428d2ac0bd418a
Aditya Thakur <ast.thakur@gmail.com>
2023-01-15 14:15:35 -0500

    Time: 165 ms (74.70%), Space: 30 MB (19.97%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,40 +1,25 @@
-
-# Difference in list lengths
 class Solution:
 
-	def calculateListLength(self, head):
-	
-		currPointer, resultLength = head, 0
-
-		while currPointer:
-
-			resultLength += 1
-
-			currPointer = currPointer.next
-
-		return resultLength
-
 	def getIntersectionNode(self, headFirstList, headSecondList):
-	
-		firstListLength, secondListLength = self.calculateListLength(headFirstList), self.calculateListLength(headSecondList)
-	
-		currFirstList, currSecondList = headFirstList, headSecondList
-	
-		if firstListLength > secondListLength:
-			
-			for _ in range(firstListLength-secondListLength):
-	
-				currFirstList = currFirstList.next
-			
-		else:
-	
-			for _ in range(secondListLength-firstListLength):
-	
-				currSecondList = currSecondList.next
-	
-		while currFirstList != currSecondList:
-	
+
+		visitedNodes = set()
+
+		currFirstList = headFirstList	
+
+		while currFirstList:
+
+			visitedNodes.add(currFirstList)
+
 			currFirstList = currFirstList.next
+
+		currSecondList = headSecondList
+
+		while currSecondList:
+
+			if currSecondList in visitedNodes:
+
+				return currSecondList
+
 			currSecondList = currSecondList.next
-	
-		return currFirstList
+
+		return
