52c802585b0a524cb8678d637744b8a814e2ffc5
Aditya Thakur <ast.thakur@gmail.com>
2023-01-15 16:32:12 -0500

    Time: 148 ms (97.86%), Space: 29.5 MB (68.47%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,38 +1,13 @@
 class Solution:
 
-	def calculateLength(self, head):
-
-		result, currNode = 0, head
-
-		while currNode:
-
-			result += 1
-
-			currNode = currNode.next
-
-		return result
-
 	def getIntersectionNode(self, headFirstList, headSecondList):
-
-		firstListLength, secondListLength = self.calculateLength(headFirstList), self.calculateLength(headSecondList)
 
 		currFirstList, currSecondList = headFirstList, headSecondList
 
-		if firstListLength >= secondListLength:
-
-			for _ in range(firstListLength-secondListLength):
-
-				currFirstList = currFirstList.next
-
-		else:
-
-			for _ in range(secondListLength-firstListLength):
-
-				currSecondList = currSecondList.next
-
 		while currFirstList != currSecondList:
 
-			currFirstList = currFirstList.next
-			currSecondList = currSecondList.next
+			currFirstList = currFirstList.next if currFirstList else headSecondList
+
+			currSecondList = currSecondList.next if currSecondList else headFirstList
 
 		return currFirstList
