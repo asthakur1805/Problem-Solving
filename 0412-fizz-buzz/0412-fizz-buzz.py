277c748a2a6787bf7db35d1247ea3d71692f0028
Aditya Thakur <ast.thakur@gmail.com>
2023-01-18 18:47:46 -0500

    Time: 47 ms (66.62%), Space: 15 MB (37.48%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -4,13 +4,35 @@ class Solution:
 
 		result = []
 
+		fizz, buzz = 1, 1
+
 		for num in range(1, inputEnd+1):
-		
-			isFizz = 'Fizz' if not num % 3 else ''
 
-			isBuzz = 'Buzz' if not num % 5 else ''
+			if fizz == 3 and buzz == 5:
 
-			result.append(str(num) if not isFizz and not isBuzz else f'{isFizz}{isBuzz}')
+				result.append('FizzBuzz')
+
+				fizz, buzz = 0, 0
+
+			elif fizz == 3:
+
+				result.append('Fizz')
+
+				fizz = 0
+
+			elif buzz == 5:
+
+				result.append('Buzz')
+
+				buzz = 0
+
+			else:
+
+				result.append(str(num))
+
+
+			fizz += 1
+
+			buzz += 1
 
 		return result
-			
