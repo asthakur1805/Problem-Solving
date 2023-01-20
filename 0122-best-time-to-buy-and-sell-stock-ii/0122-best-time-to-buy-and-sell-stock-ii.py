2049edc505f65830827f8c097393a0408002277f
Aditya Thakur <ast.thakur@gmail.com>
2023-01-19 20:48:42 -0500

    Time: 69 ms (63.18%), Space: 15.2 MB (22.57%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,18 +2,16 @@ class Solution:
 
 	def maxProfit(self, prices):
 
-		resultProfit = 0
-
 		buyPointer = 0
+
+		resultProfit = 0
 
 		for sellPointer in range(1, len(prices)):
 
-			if prices[buyPointer] < prices[sellPointer]:
+			if prices[sellPointer] > prices[buyPointer]:
 
-				resultProfit += (prices[sellPointer] - prices[buyPointer])
+				resultProfit += prices[sellPointer] - prices[buyPointer]
 
 			buyPointer = sellPointer
 
 		return resultProfit
-
-				
