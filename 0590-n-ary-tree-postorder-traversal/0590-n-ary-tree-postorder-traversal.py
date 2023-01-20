cfe0d8c7dca11fb82c933f17cb0c5d3c90bbfc66
Aditya Thakur <ast.thakur@gmail.com>
2023-01-19 19:41:39 -0500

    Time: 63 ms (40.51%), Space: 16 MB (97.81%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -2,20 +2,33 @@ class Solution:
 
 	def postorder(self, root):
 
+		if not root:
+
+			return
+
 		result = []
 
-		self.helper(root, result)
+		stack = [(root, False)]
+
+		while stack:
+
+			node, toProcess = stack.pop()
+
+			if not toProcess:
+
+				stack.append((node, True))
+
+				for childIndex in range(len(node.children)-1,-1,-1):
+
+					childNode = node.children[childIndex]
+
+					stack.append((childNode, False))
+
+			else:
+
+				result.append(node.val)
 
 		return result
 
-	def helper(self, node, result):
-
-		if not node:
-
-			return 
-
-		for childNode in node.children:
-
-			self.helper(childNode, result)
-
-		result.append(node.val)
+			
+		
