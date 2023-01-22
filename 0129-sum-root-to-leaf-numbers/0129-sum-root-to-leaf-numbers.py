c89bbe45ece1920cd4e06fc38c84a0a02e184f43
Aditya Thakur <ast.thakur@gmail.com>
2023-01-21 19:16:10 -0500

    Time: 65 ms (12.06%), Space: 14 MB (17.74%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,29 @@
+class Solution:
+
+	def sumNumbers(self, root):
+
+		self.resultSum = 0
+
+		self.helper(root, 0)
+
+		return self.resultSum
+
+	def helper(self, node, pathNumber):
+
+		if not node:
+
+			return
+
+		pathNumber = pathNumber * 10 + node.val
+
+		if not node.left and not node.right:
+
+			self.resultSum += pathNumber
+
+			return
+
+		self.helper(node.left, pathNumber)
+
+		self.helper(node.right, pathNumber)
+
+		
