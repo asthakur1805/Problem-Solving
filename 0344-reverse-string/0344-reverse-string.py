b7f58d988e5d58ca02d3a244e61633a041ac83df
Aditya Thakur <ast.thakur@gmail.com>
2023-01-27 17:28:58 -0500

    Time: 625 ms (5.06%), Space: 18.7 MB (11.55%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -2,17 +2,10 @@ class Solution:
 
 	def reverseString(self, inputStr):
 
-		self.helper(inputStr, 0, len(inputStr)-1)
+		leftPointer, rightPointer = 0, len(inputStr)-1
 
-	def helper(self, inputStr, leftPointer, rightPointer):
+		while leftPointer < rightPointer:
 
-		if leftPointer >= rightPointer:
+			inputStr[leftPointer], inputStr[rightPointer] = inputStr[rightPointer], inputStr[leftPointer]
 
-			return
-
-		inputStr[leftPointer], inputStr[rightPointer] = inputStr[rightPointer], inputStr[leftPointer]
-
-		self.helper(inputStr, leftPointer + 1, rightPointer - 1)
-
-		
-	
+			leftPointer, rightPointer = leftPointer + 1, rightPointer - 1
