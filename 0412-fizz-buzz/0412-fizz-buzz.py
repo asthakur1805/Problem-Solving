84979b21ca75633aea80ad15c931b5ae3d4bf6c9
Aditya Thakur <ast.thakur@gmail.com>
2023-01-27 18:50:49 -0500

    Time: 46 ms (68.02%), Space: 15 MB (36.41%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,38 +1,25 @@
 class Solution:
 
-	def fizzBuzz(self, inputEnd):
+	def fizzBuzz(self, upperLimit):
 
 		result = []
 
-		fizz, buzz = 1, 1
+		for num in range(1, upperLimit+1):
 
-		for num in range(1, inputEnd+1):
-
-			if fizz == 3 and buzz == 5:
+			if not num % 3 and not num % 5:
 
 				result.append('FizzBuzz')
 
-				fizz, buzz = 0, 0
-
-			elif fizz == 3:
+			elif not num % 3:
 
 				result.append('Fizz')
 
-				fizz = 0
-
-			elif buzz == 5:
+			elif not num % 5:
 
 				result.append('Buzz')
-
-				buzz = 0
 
 			else:
 
 				result.append(str(num))
-
-
-			fizz += 1
-
-			buzz += 1
 
 		return result
