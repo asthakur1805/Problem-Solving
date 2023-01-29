e4a6d7818c87687c1a4362d0753091765ec5f250
Aditya Thakur <ast.thakur@gmail.com>
2023-01-29 15:02:04 -0500

    Time: 248 ms (54.55%), Space: 25.3 MB (76.85%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,23 +1,13 @@
 class Solution:
 
-	def rotate(self, nums, rotations):
+	def rotate(self, nums, numRotations):
 
-		numsLength = len(nums)
+		clone = nums.copy()
 
-		rotations %= numsLength
+		for index in range(len(nums)):
 
-		self.reverse(nums, 0, numsLength - 1)
+			clone[(index+numRotations)%len(nums)] = nums[index]
 
-		self.reverse(nums, 0, rotations-1)
+		for index in range(len(nums)):
 
-		self.reverse(nums, rotations, numsLength-1)
-		
-	def reverse(self, nums, leftPointer, rightPointer):
-
-		while leftPointer < rightPointer:
-
-			nums[leftPointer], nums[rightPointer] = nums[rightPointer], nums[leftPointer]
-
-			leftPointer, rightPointer = leftPointer + 1 , rightPointer - 1
-
-	
+			nums[index] = clone[index]
