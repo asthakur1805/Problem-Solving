d79513452d9c2c0c22b7f2fff6753b479a6fe8ec
Aditya Thakur <ast.thakur@gmail.com>
2023-01-31 11:25:14 -0500

    Time: 85 ms (61.84%), Space: 18.8 MB (63.01%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,21 @@
+class Solution:
+
+	def lowestCommonAncestor(self, root, firstNode, secondNode):
+
+		curr = root
+
+		while True:
+
+			if firstNode.val < curr.val and secondNode.val < curr.val:
+
+				curr = curr.left
+
+			elif firstNode.val > curr.val and secondNode.val > curr.val:
+
+				curr = curr.right
+
+			else:
+
+				return curr
+
+		
