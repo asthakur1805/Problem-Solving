1cba7655bef66acdd55789a0541902c6ae112ba2
Aditya Thakur <ast.thakur@gmail.com>
2023-02-11 16:28:51 -0500

    Time: 169 ms (85.96%), Space: 20.4 MB (78.07%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -2,22 +2,12 @@ class Solution:
 
 	def searchMatrix(self, matrix, target):
 
-		rowPointer, columnPointer = len(matrix)-1, 0
+		for row in range(len(matrix)):
 
-		while rowPointer >= 0 and columnPointer <= len(matrix[0])-1:
+			for column in range(len(matrix[row])):
 
-			if matrix[rowPointer][columnPointer] == target:
+				if matrix[row][column] == target:
 
-				return True
-
-			if matrix[rowPointer][columnPointer] > target:
-
-				rowPointer -= 1
-
-			else:
-
-				columnPointer += 1
+					return True
 
 		return False
-
-		
