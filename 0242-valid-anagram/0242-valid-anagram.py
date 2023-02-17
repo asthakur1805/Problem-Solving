e166a5d19599d4fe45eea7cfc05a9e67cbd24854
Aditya Thakur <ast.thakur@gmail.com>
2023-02-17 17:10:20 -0500

    Time: 43 ms (88.62%), Space: 14.4 MB (61.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,16 +2,5 @@ class Solution:
 
 	def isAnagram(self, firstStr, secondStr):
 
-		countsFirstStr, countsSecondStr = [0]*26,[0]*26
-
-		for char in firstStr:
-
-			countsFirstStr[ord(char)-ord('a')] += 1
-
-		for char in secondStr:
-
-			countsSecondStr[ord(char)-ord('a')] += 1
-
-		return countsFirstStr == countsSecondStr
-
+		return Counter(firstStr) == Counter(secondStr)
 			
