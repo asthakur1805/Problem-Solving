693dfa6a3014230739fcbb709c725300e9a559cd
Aditya Thakur <ast.thakur@gmail.com>
2023-02-18 15:21:43 -0500

    Time: 34 ms (98.23%), Space: 14.5 MB (61.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,16 +2,6 @@ class Solution:
 
 	def isAnagram(self, firstStr, secondStr):
 
-		if len(firstStr) != len(secondStr):
+		from collections import Counter
 
-			return False
-
-		countsFirstStr, countsSecondStr = [0]*26, [0]*26
-
-		for index in range(len(firstStr)):
-
-			countsFirstStr[ord(firstStr[index])-ord('a')] += 1
-
-			countsSecondStr[ord(secondStr[index])-ord('a')] += 1
-
-		return countsFirstStr == countsSecondStr
+		return Counter(firstStr) == Counter(secondStr)
