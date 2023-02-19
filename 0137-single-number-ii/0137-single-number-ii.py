8263f15c1496a7cfa779d90d8e12d4cc63718e8f
Aditya Thakur <ast.thakur@gmail.com>
2023-02-18 22:08:21 -0500

    Time: 100 ms (39.83%), Space: 15.7 MB (62.40%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -2,18 +2,28 @@ class Solution:
 
 	def singleNumber(self, nums):
 
-		if len(nums) == 1:
+		result = 0
 
-			return nums[0]
+		for bitPosition in range(32):
 
-		nums.sort()
+			mask = (1 << bitPosition)
 
-		if nums[-1] > nums[-2]:
+			countSetBits = 0
 
-			return nums[-1]
+			for num in nums:
 
-		for index in range(1,len(nums),3):
+				if num & mask:
 
-			if nums[index] > nums[index-1]:
+					countSetBits += 1
 
-				return nums[index-1]
+			if countSetBits % 3:
+
+				if result + mask >= (1 << 31):
+
+					result += mask - (1 << 32)
+
+				else:
+
+					result += mask
+
+		return result
