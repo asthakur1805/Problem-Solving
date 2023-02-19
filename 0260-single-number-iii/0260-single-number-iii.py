b4dbf62b8f10ef03d929bda406e103286ea9db66
Aditya Thakur <ast.thakur@gmail.com>
2023-02-19 17:30:33 -0500

    Time: 57 ms (90.60%), Space: 16.2 MB (32.07%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -2,24 +2,18 @@ class Solution:
 
 	def singleNumber(self, nums):
 
-		xorResult = 0
+		numCounts = {}
 
 		for num in nums:
 
-			xorResult ^= num
+			numCounts[num] = numCounts.get(num, 0) + 1
 
-		mask = xorResult & (-xorResult)
+		result = []
 
-		result = [0, 0]
+		for num, count in numCounts.items():
 
-		for num in nums:
+			if count == 1:
 
-			if num & mask:
-
-				result[0] ^= num
-
-			else:
-
-				result[1] ^= num
+				result.append(num)
 
 		return result
