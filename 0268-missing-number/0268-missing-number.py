447c1d0455b4e8374e56713cc3910ce6d0c55e58
Aditya Thakur <ast.thakur@gmail.com>
2023-02-20 12:38:12 -0500

    Time: 144 ms (52.51%), Space: 15.1 MB (98.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -6,6 +6,6 @@ class Solution:
 
 		for index in range(len(inputNums)):
 
-			result += index - inputNums[index]
+			result ^= index ^ inputNums[index]
 
-		return result + len(inputNums)
+		return result ^ len(inputNums)
