ee381f54068de441989ec11c4833258c9e0cb7fd
Aditya Thakur <ast.thakur@gmail.com>
2023-03-12 15:27:55 -0400

    Time: 40 ms (15.87%), Space: 13.8 MB (49.91%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -2,18 +2,20 @@ class Solution:
 
 	def getRow(self, inputIndex):
 
-		triangle = [[1]]
+		rowResult = [1]
 
-		for rowIndex in range(1, inputIndex + 1):
+		for rowIndex in range(1,inputIndex+1):
 
-			rowResult = [1]
+			columnIndex = len(rowResult) - 1
 
-			for columnIndex in range(1, rowIndex):
+			for _ in range(rowIndex-1):
 
-				rowResult.append(triangle[rowIndex-1][columnIndex] + triangle[rowIndex-1][columnIndex-1])
+				rowResult[columnIndex] += rowResult[columnIndex-1]
+
+				columnIndex -= 1
 
 			rowResult.append(1)
 
-			triangle.append(rowResult)
+		return rowResult
 
-		return triangle[inputIndex]
+		
