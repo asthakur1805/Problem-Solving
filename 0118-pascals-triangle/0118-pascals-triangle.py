655eddd7985f84f1c94b22e8bb0c52f6af20842c
Aditya Thakur <ast.thakur@gmail.com>
2023-03-12 14:36:39 -0400

    Time: 38 ms (42.81%), Space: 13.9 MB (55.89%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -2,22 +2,18 @@ class Solution:
 
 	def generate(self, numRows):
 
-		if numRows < 1:
+		resultTriangle = [[1]]
 
-			return
-
-		result = [[1]]
-
-		for rowIndex in range(1, numRows):
+		for rowIndex in range(1,numRows):
 
 			rowResult = [1]
 
 			for columnIndex in range(1, rowIndex):
 
-				rowResult.append(result[rowIndex-1][columnIndex] + result[rowIndex-1][columnIndex-1])
+				rowResult.append(resultTriangle[rowIndex-1][columnIndex] + resultTriangle[rowIndex-1][columnIndex-1])
 
 			rowResult.append(1)
 
-			result.append(rowResult)
+			resultTriangle.append(rowResult)
 
-		return result
+		return resultTriangle
