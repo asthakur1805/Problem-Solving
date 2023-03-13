884669666b93dfa82289878e310ccc19300cf954
Aditya Thakur <ast.thakur@gmail.com>
2023-03-12 21:45:57 -0400

    Time: 40 ms (14.19%), Space: 13.8 MB (93.84%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -2,33 +2,21 @@ class Solution:
 
 	def preorderTraversal(self, root):
 
-		if not root:
+		result = []
+
+		self.helper(root, result)
+
+		return result
+
+	def helper(self, node, result):
+
+		if not node:
 
 			return
 
-		stack = [(root, False)]
+		result.append(node.val)
 
-		result = []
+		self.helper(node.left, result)
 
-		while stack:
+		self.helper(node.right, result)
 
-			node, toProcess = stack.pop()
-
-			if not toProcess:
-
-				if node.right:
-
-					stack.append((node.right, False))
-
-				if node.left:
-
-					stack.append((node.left, False))
-
-				stack.append((node, True))
-
-			else:
-
-				result.append(node.val)
-
-
-		return result
