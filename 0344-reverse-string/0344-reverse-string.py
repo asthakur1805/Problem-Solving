ab9ea19d8521f31b6e5803a79ef67b1983865b92
Aditya Thakur <ast.thakur@gmail.com>
2023-03-30 18:01:06 -0400

    Time: 207 ms (32.15%), Space: 18.4 MB (36.25%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -2,13 +2,16 @@ class Solution:
 
 	def reverseString(self, inputStr):
 
-		left, right = 0, len(inputStr)-1
+		stack = []
 
-		while left < right:
+		for char in inputStr:
 
-			inputStr[left], inputStr[right] = inputStr[right], inputStr[left]
-			left += 1
-			right -= 1
+			stack.append(char)
 
+		for index in range(len(inputStr)):
 
-		
+			inputStr[index] = stack.pop()
+
+			
+
+			
