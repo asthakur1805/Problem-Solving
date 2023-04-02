c2e2e2b17427f032318d067fdf17f79f68f30851
Aditya Thakur <ast.thakur@gmail.com>
2023-04-02 18:05:54 -0400

    Time: 35 ms (53.67%), Space: 13.8 MB (93.99%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,22 @@
+class Solution:
+
+	def plusOne(self, digits):
+
+		for index in range(len(digits)-1,-1,-1):
+
+			if digits[index] < 9:
+
+				digits[index] += 1
+
+				break
+	
+			else:
+
+				digits[index] = 0
+
+		else:
+
+			digits.insert(0,1)
+
+		return digits
+
