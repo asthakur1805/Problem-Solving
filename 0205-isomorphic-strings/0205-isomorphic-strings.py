b241b738658e54f7ca8daf451f1125dc69257830
Aditya Thakur <ast.thakur@gmail.com>
2023-04-02 10:16:31 -0400

    Time: 52 ms (20.70%), Space: 14.2 MB (81.13%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,11 +2,7 @@ class Solution:
 
 	def isIsomorphic(self, firstStr, secondStr):
 
-		if len(firstStr) != len(secondStr):
-
-			return False
-
-		firstMap, secondMap = [0] * 256, [0] * 256
+		firstMap, secondMap = [0]*255, [0]*255
 
 		for index, (firstChar, secondChar) in enumerate(zip(firstStr, secondStr)):
 
@@ -14,7 +10,6 @@ class Solution:
 
 				return False
 
-			firstMap[ord(firstChar)], secondMap[ord(secondChar)] = index + 1, index + 1
+			firstMap[ord(firstChar)], secondMap[ord(secondChar)] = index+1, index+1
 
 		return True
-			
