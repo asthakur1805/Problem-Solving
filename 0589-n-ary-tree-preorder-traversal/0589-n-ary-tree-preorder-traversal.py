6663449bb2e97be206682c56ec9d0c980ab2f97e
Aditya Thakur <ast.thakur@gmail.com>
2023-04-02 20:36:41 -0400

    Time: 64 ms (6.47%), Space: 16.3 MB (38.03%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -2,30 +2,20 @@ class Solution:
 
 	def preorder(self, root):
 
-		if not root:
+		result = []
+
+		self.helper(root, result)
+
+		return result
+
+	def helper(self, node, result):
+
+		if not node:
 
 			return
 
-		result = []
+		result.append(node.val)
 
-		stack = [(root, False)]
+		for childNode in node.children:
 
-		while stack:
-
-			node, toProcess = stack.pop()
-
-			if not toProcess:
-
-				for childIndex in range(len(node.children)-1,-1,-1):
-
-					childNode = node.children[childIndex]
-
-					stack.append((childNode, False))
-
-				stack.append((node, True))
-
-			else:
-
-				result.append(node.val)
-
-		return result
+			self.helper(childNode, result)
