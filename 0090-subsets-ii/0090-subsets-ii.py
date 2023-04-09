889cc9eddb762887d2ae6e2e3da8165bfbcad7be
Aditya Thakur <ast.thakur@gmail.com>
2023-04-09 14:00:59 -0400

    Time: 40 ms (56.55%), Space: 14.2 MB (37.85%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,32 +4,27 @@ class Solution:
 
 		nums.sort()
 
-		index = 0
+		index, builder, result = 0, [], []
 
-		currSubset = []
-
-		result = []
-
-		self.helper(nums, index, currSubset, result)
+		self.helper(nums, index, builder, result)
 
 		return result
 
-	def helper(self, nums, index, currSubset, result):
+	def helper(self, nums, index, builder, result):
 
 		if index == len(nums):
 
-			result.append(currSubset.copy())
+			result.append(builder.copy())
 			return
 
-		currSubset.append(nums[index])
+		builder.append(nums[index])
 
-		self.helper(nums, index+1, currSubset, result)
+		self.helper(nums, index+1, builder, result)
 
-		currSubset.pop()
+		builder.pop()
 
 		while index < len(nums)-1 and nums[index] == nums[index+1]:
+
 			index += 1
 
-		self.helper(nums, index+1, currSubset, result)
-
-		
+		self.helper(nums, index+1, builder, result)
