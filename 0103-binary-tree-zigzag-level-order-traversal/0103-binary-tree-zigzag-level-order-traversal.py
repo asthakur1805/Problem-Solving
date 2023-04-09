f2815b8f713864f8a047e2ac32b119560b3c3fcc
Aditya Thakur <ast.thakur@gmail.com>
2023-04-09 12:30:31 -0400

    Time: 33 ms (72.31%), Space: 14 MB (99.95%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -3,7 +3,8 @@ class Solution:
 	def zigzagLevelOrder(self, root):
 
 		if not root:
-			return
+
+			return 
 
 		result = []
 
@@ -13,23 +14,7 @@ class Solution:
 
 			level = []
 
-			if len(result) % 2:
-
-				for _ in range(len(queue)):
-
-					node = queue.pop()
-
-					level.append(node.val)
-
-					if node.right:
-
-						queue.appendleft(node.right)
-
-					if node.left:
-
-						queue.appendleft(node.left)
-
-			else:
+			if not len(result) % 2:
 
 				for _ in range(len(queue)):
 
@@ -45,6 +30,21 @@ class Solution:
 
 						queue.append(node.right)
 
+			else:
+
+				for _ in range(len(queue)):
+
+					node = queue.pop()
+
+					level.append(node.val)
+
+					if node.right:
+
+						queue.appendleft(node.right)
+
+					if node.left:
+
+						queue.appendleft(node.left)
 
 			result.append(level)
 
