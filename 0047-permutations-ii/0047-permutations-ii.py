f95ac6857df95e7cb09eeee1f4e46cfee71340d4
Aditya Thakur <ast.thakur@gmail.com>
2023-04-09 20:46:46 -0400

    Time: 51 ms (96.54%), Space: 14.2 MB (72.90%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -19,10 +19,9 @@ class Solution:
 
 		for swapIndex in range(index,len(nums)):
 
-			if swapIndex != index and nums[swapIndex] == nums[index]:
+			if swapIndex == index or nums[swapIndex] != nums[index]:
 
-				continue
 
-			nums[index], nums[swapIndex] = nums[swapIndex], nums[index]
+			    nums[index], nums[swapIndex] = nums[swapIndex], nums[index]
 
-			self.helper(nums.copy(), index+1, result)
+			    self.helper(nums.copy(), index+1, result)   
