c0cb7a5e508f8c0a837b68d20fc668433a47ec68
Aditya Thakur <ast.thakur@gmail.com>
2023-04-21 21:36:10 -0400

    Time: 165 ms (66.53%), Space: 15.5 MB (22.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,17 +2,14 @@ class Solution:
 
 	def majorityElement(self, nums):
 
-		maxCount, result = 0, None
-
-		counts = {}
+		count, result = 0, None
 
 		for num in nums:
 
-			counts[num] = counts.get(num, 0) + 1
+			if count == 0:
 
-			if counts[num] > maxCount:
-
-				maxCount = counts[num]
 				result = num
+
+			count += (1 if result == num else -1)
 
 		return result
