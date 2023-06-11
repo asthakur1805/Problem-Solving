27beb776a79508d92d947b137afb123ce21ee4f1
Aditya Thakur <ast.thakur@gmail.com>
2023-06-11 16:15:52 -0400

    Time: 140 ms (53.03%), Space: 17.2 MB (42.30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,32 +4,22 @@ class Solution:
 
 		numRows, numColumns = len(matrix), len(matrix[0])
 
+		setRows, setColumns = [False]*numRows, [False]*numColumns
+
 		for currRow in range(numRows):
 
 			for currColumn in range(numColumns):
 
 				if matrix[currRow][currColumn] == 0:
 
-					for zeroRow in range(numRows):
-
-						if matrix[zeroRow][currColumn] != 0:
-
-							matrix[zeroRow][currColumn] = None
-
-					for zeroColumn in range(numColumns):
-
-						if matrix[currRow][zeroColumn] != 0:
-
-							matrix[currRow][zeroColumn] = None
+					setRows[currRow], setColumns[currColumn] = True, True
 
 		for currRow in range(numRows):
 
 			for currColumn in range(numColumns):
 
-				if matrix[currRow][currColumn] == None:
+				if setRows[currRow] or setColumns[currColumn]:
 
 					matrix[currRow][currColumn] = 0
 
-						
-
-						
+	
