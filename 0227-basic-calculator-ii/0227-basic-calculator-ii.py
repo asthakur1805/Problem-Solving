dbd4353e231f8f695597655c414899e49cec3495
Aditya Thakur <ast.thakur@gmail.com>
2023-06-16 15:24:09 -0400

    Time: 239 ms (23.91%), Space: 18.7 MB (70.88%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,52 +2,45 @@ class Solution:
 
 	def calculate(self, inputStr):
 
-		stack = []
+		tempResult = 0
+	
+		operand = 0
 
 		prevOperator = '+'
 
-		secondOperand = 0
+		result = 0
 
 		for index, char in enumerate(inputStr):
 
 			if self.isDigit(char):
 
-				secondOperand = secondOperand * 10 + ord(char) - ord('0')
+				operand = operand * 10 + ord(char) - ord('0')
 
-			if index == len(inputStr)-1 or (not self.isDigit(char) and char != ' '):
+			if index == len(inputStr)-1 or char in ('+','-','*','/'):
 
 				if prevOperator == '+':
 
-					stack.append(secondOperand)
-	
+					result += tempResult
+					tempResult = operand
+
 				elif prevOperator == '-':
 
-					stack.append(-secondOperand)
+					result += tempResult
+					tempResult = -operand
 
 				elif prevOperator == '*':
 
-					firstOperand = stack.pop()
-	
-					stack.append(firstOperand * secondOperand)
+					tempResult *= operand
 
-				elif prevOperator == '/':
+				else:
 
-					firstOperand = stack.pop()
+					tempResult = int(tempResult / operand)
 
-					stack.append(int(firstOperand / secondOperand))
+				operand, prevOperator = 0, char
 
-				secondOperand = 0
-				prevOperator = char
-
-		result = 0
-
-		for num in stack:
-
-			result += num
+		result += tempResult
 
 		return result
-
-				
 
 	def isDigit(self, char):
 
