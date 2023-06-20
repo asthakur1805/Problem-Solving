3caca8246a44264ca8efc9f529eebbc0a2ade7d5
Aditya Thakur <ast.thakur@gmail.com>
2023-06-20 14:44:10 -0400

    Time: 244 ms (11.63%), Space: 17.1 MB (34.18%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+class Solution:
+
+	def findCircleNum(self, isConnected):
+
+		visited = set()
+		count = 0
+
+		for startNode in range(len(isConnected)):
+
+			if startNode not in visited:
+				
+				count += 1
+				visited.add(startNode)
+				self.bfs(isConnected, startNode, visited)
+
+		return count
+
+	def bfs(self, isConnected, startNode, visited):
+
+		queue = deque([startNode])
+
+		while queue:
+
+			for _ in range(len(queue)):
+
+				currNode = queue.popleft()
+
+				for otherNode in range(len(isConnected)):
+
+					if isConnected[currNode][otherNode] and otherNode not in visited:
+
+						queue.append(otherNode)
+						visited.add(otherNode)
+
+
+				
