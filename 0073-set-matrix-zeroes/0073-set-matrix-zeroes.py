f44b00a819de8bb469e20a1cf47764e5aed17832
Aditya Thakur <ast.thakur@gmail.com>
2023-06-23 00:44:33 -0400

    Time: 140 ms (61.08%), Space: 17.2 MB (52.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -2,9 +2,7 @@ class Solution:
 
 	def setZeroes(self, matrix):
 
-		numRows, numColumns = len(matrix), len(matrix[0])
-
-		setRows, setColumns = [1]*numRows, [1]*numColumns
+		numRows, numColumns, firstColumn = len(matrix), len(matrix[0]), 1
 
 		for currRow in range(numRows):
 
@@ -12,13 +10,36 @@ class Solution:
 
 				if matrix[currRow][currColumn] == 0:
 
-					setRows[currRow] = 0
-					setColumns[currColumn] = 0
+					matrix[currRow][0] = 0
 
-		for currRow in range(numRows):
+					if currColumn > 0:
 
-			for currColumn in range(numColumns):
+						matrix[0][currColumn] = 0
 
-				if setRows[currRow] == 0 or setColumns[currColumn] == 0:
-				
+					else:
+
+						firstColumn = 0
+
+		for currRow in range(1, numRows):
+
+			for currColumn in range(1, numColumns):
+
+				if matrix[0][currColumn] == 0 or matrix[currRow][0] == 0:
+
 					matrix[currRow][currColumn] = 0
+
+		if matrix[0][0] == 0:
+		
+			for currColumn in range(1, numColumns):
+
+				matrix[0][currColumn] = 0
+
+		if firstColumn == 0:
+
+			for currRow in range(numRows):
+
+				matrix[currRow][0] = 0
+
+			
+
+		
