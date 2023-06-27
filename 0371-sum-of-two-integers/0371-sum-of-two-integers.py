6a154ce2a02e5405ffcbbbda35a677b644f07723
Aditya Thakur <ast.thakur@gmail.com>
2023-06-27 12:36:57 -0400

    Time: 46 ms (57.62%), Space: 16.2 MB (69.78%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -2,14 +2,12 @@ class Solution:
 
 	def getSum(self, firstNumber, secondNumber):
 
-		maxPositiveLimit, mask = 0x7FFFFFFF, 0xFFFFFFFF
+		mask, INT_MAX = 0xFFFFFFFF, 0x7FFFFFFF
 
 		while secondNumber:
 
 			carry = (firstNumber & secondNumber) << 1
-
 			firstNumber = (firstNumber ^ secondNumber) & mask
-
 			secondNumber = carry & mask
 
-		return firstNumber if firstNumber <= maxPositiveLimit else (firstNumber | ~mask)
+		return firstNumber if firstNumber <= INT_MAX else firstNumber | (~mask)
