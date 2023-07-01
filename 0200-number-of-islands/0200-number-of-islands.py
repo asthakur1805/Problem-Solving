e5ed16151040eb1f66c7c37e6e5b0490d85463bb
Aditya Thakur <ast.thakur@gmail.com>
2023-07-01 16:03:01 -0400

    Time: 347 ms (53.65%), Space: 24.2 MB (23.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,40 +1,28 @@
-from collections import deque
-
 class Solution:
 
 	def numIslands(self, grid):
 
-		numRows, numColumns, count, visited, directions = len(grid), len(grid[0]), 0, set(), [(0,1),(0,-1),(1,0),(-1,0)]
+		numberRows, numberColumns, count, visited, directions = len(grid), len(grid[0]), 0, set(), [(0,1),(1,0),(0,-1),(-1,0)]
 
-		for startRow in range(numRows):
+		for currRow in range(numberRows):
 
-			for startColumn in range(numColumns):
+			for currColumn in range(numberColumns):
 
-				if grid[startRow][startColumn] == '1' and (startRow, startColumn) not in visited:
+				if grid[currRow][currColumn] == "1" and (currRow,currColumn) not in visited:
 
 					count += 1
-					self.bfs(grid, startRow, startColumn, numRows, numColumns, visited, directions)
+					self.dfs(grid, currRow, currColumn, numberRows, numberColumns, visited, directions)
 
 		return count
 
-	def bfs(self, grid, startRow, startColumn, numRows, numColumns, visited, directions):
+	def dfs(self, grid, currRow, currColumn, numberRows, numberColumns, visited, directions):
 
-		visited.add((startRow,startColumn))
-		queue = deque([(startRow,startColumn)])
+		visited.add((currRow, currColumn))
 
-		while queue:
+		for rowDirection, columnDirection in directions:
 
-			for _ in range(len(queue)):
+			neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
 
-				currRow, currColumn = queue.popleft()
+			if 0 <= neighborRow < numberRows and 0 <= neighborColumn < numberColumns and (neighborRow,neighborColumn) not in visited and grid[neighborRow][neighborColumn] == "1":
 
-				for rowDirection, columnDirection in directions:
-
-					neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
-				
-					if 0 <= neighborRow < numRows and 0 <= neighborColumn < numColumns and grid[neighborRow][neighborColumn] == '1' and (neighborRow,neighborColumn) not in visited:
-
-						visited.add((neighborRow,neighborColumn))
-						queue.append((neighborRow,neighborColumn))
-
-			
+				self.dfs(grid, neighborRow, neighborColumn, numberRows, numberColumns, visited, directions)
