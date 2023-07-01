76b5efb9949a779740391236fa5aa6c45a2c07fc
Aditya Thakur <ast.thakur@gmail.com>
2023-07-01 16:23:18 -0400

    Time: 67 ms (62.50%), Space: 16.4 MB (25.54%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -6,17 +6,17 @@ class Solution:
 
 		# 0: Empty, 1: Fresh, 2: Rotten
 
-		numRows, numColumns, numFresh, directions, queue = len(grid), len(grid[0]), 0, [(-1,0), (1,0), (0,-1), (0,1)], deque([])
+		numRows, numColumns, numFresh, queue, directions = len(grid), len(grid[0]), 0, deque([]), [(0,1),(0,-1),(1,0),(-1,0)]
 
-		for row in range(numRows):
+		for currRow in range(numRows):
 
-			for column in range(numColumns):
+			for currColumn in range(numColumns):
 
-				if grid[row][column] == 2:
+				if grid[currRow][currColumn] == 2:
 
-					queue.append((row, column))
+					queue.append((currRow, currColumn))
 
-				elif grid[row][column] == 1:
+				elif grid[currRow][currColumn] == 1:
 
 					numFresh += 1
 
@@ -34,17 +34,15 @@ class Solution:
 
 					if 0 <= neighborRow < numRows and 0 <= neighborColumn < numColumns and grid[neighborRow][neighborColumn] == 1:
 
-						numFresh -= 1
 						queue.append((neighborRow, neighborColumn))
+
 						grid[neighborRow][neighborColumn] = 2
+	
+						numFresh -= 1
 
 			time += 1
 
 		return time if numFresh == 0 else -1
-
-		
-
-			
 
 		
 
