2015426ce32e0ef6281e7b5f134339d24e16388f
Aditya Thakur <ast.thakur@gmail.com>
2023-07-04 20:54:47 -0400

    Time: 127 ms (23.64%), Space: 20.9 MB (98.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,13 +2,37 @@ class Solution:
 
 	def topKFrequent(self, nums, K):
 
-		counts = {}
+		maxCount, counts = 0, {}
 
 		for num in nums:
 
 			counts[num] = counts.get(num,0) + 1
 
-		result = sorted(counts.items(),reverse=True,key=lambda count:count[1])
+			if counts[num] > maxCount:
 
-		return [result[index][0] for index in range(K)]
+				maxCount = counts[num]
+
+		buckets = [[] for _ in range(maxCount+1)]
+
+		for num,count in counts.items():
+
+			buckets[count].append(num)
+
+		result = []
+
+		while True:
+
+			for count in range(maxCount,0,-1):
+
+				bucketNums = buckets[count]
+
+				for num in bucketNums:
+
+					result.append(num)
+
+					K -= 1
+
+					if K == 0:
 		
+						return result
+
