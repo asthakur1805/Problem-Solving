44d8b2f2e1a12bce276117bd9ca0e055f1bff1d1
Aditya Thakur <ast.thakur@gmail.com>
2023-07-10 13:49:05 -0400

    Time: 658 ms (77.09%), Space: 23.5 MB (89.49%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,45 @@
+class Solution:
+
+	def eventualSafeNodes(self, graph):
+
+		visited = [0] * len(graph)
+
+		for currNode in range(len(graph)):
+
+			if not visited[currNode]:
+
+				self.dfs(graph, currNode, visited)
+
+		result = []
+
+		for node in range(len(visited)):
+
+			if visited[node] == 1:
+
+				result.append(node)
+
+		return result
+
+	def dfs(self, graph, currNode, visited):
+
+		visited[currNode] = 2
+
+		for neighborNode in graph[currNode]:
+
+			if not visited[neighborNode]:
+
+				if self.dfs(graph, neighborNode, visited):
+
+					return True
+
+			elif visited[neighborNode] == 2:
+
+				return True
+
+		visited[currNode] = 1
+		return
+
+	
+	
+
+				
