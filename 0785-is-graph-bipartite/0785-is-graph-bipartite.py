6d64b6f4192496d0708c0316b2f4b2b5aaf4ebc5
Aditya Thakur <ast.thakur@gmail.com>
2023-07-16 11:06:04 -0400

    Time: 184 ms (70.54%), Space: 16.8 MB (51.66%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,41 +1,50 @@
+from collections import deque
+
 class Solution:
 
 	def isBipartite(self, graph):
 
-		colored = {}
+		color, visited = {}, set()
 
-		for startNode in range(len(graph)):
+		for startNode in range(len(graph)-1):
 
-			if startNode not in colored:
+			if startNode not in visited:
 
-				if not self.dfs(graph, startNode, colored, 0):
-
-					return False
-
-		return True
-
-	def dfs(self, graph, currNode, colored, currColor):
-
-		colored[currNode] = currColor
-
-		for neighborNode in graph[currNode]:
-
-			if neighborNode not in colored:
-
-				if not self.dfs(graph, neighborNode, colored, 1-currColor):
-
-					return False
-
-			else:
-
-				if colored[neighborNode] != 1 - currColor:
+				if not self.bfs(graph, startNode, visited, color):
 
 					return False
 
 		return True
 
-		
+	def bfs(self, graph, startNode, visited, color):
 
-		
+		visited.add(startNode)
+		queue, color[startNode],neighborColor = deque([startNode]), 0, 1
 
-				
+		while queue:
+
+			for _ in range(len(queue)):
+
+				currNode = queue.popleft()
+
+				for neighborNode in graph[currNode]:
+
+					if neighborNode not in visited:
+
+						visited.add(neighborNode)
+						queue.append(neighborNode)
+						color[neighborNode] = neighborColor
+
+					elif color[neighborNode] != neighborColor:
+
+						return False
+
+			neighborColor = 1-neighborColor
+
+		return True
+
+						
+					
+	
+
+	
