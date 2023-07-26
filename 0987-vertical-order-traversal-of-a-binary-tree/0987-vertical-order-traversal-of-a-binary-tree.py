df32bc80ba4c25b22a4560e492432dda2439b44d
Aditya Thakur <ast.thakur@gmail.com>
2023-07-26 13:16:45 -0400

    Time: 61 ms (7.05%), Space: 16.6 MB (55.39%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,50 @@
+from collections import deque, defaultdict
+
+class Solution:
+
+	def verticalTraversal(self, root):
+
+		if not root:
+
+			return
+
+		verticalNodes = defaultdict(list)
+
+		queue = deque([(root,0,0)])
+
+		minVertical, maxVertical = float('inf'), float('-inf')
+
+		while queue:
+
+			for _ in range(len(queue)):
+
+				node, vertical, level = queue.popleft()
+
+				minVertical, maxVertical = min(minVertical,vertical), max(maxVertical,vertical)	
+
+				verticalNodes[vertical].append((level,node.val))
+
+				if node.left:
+
+					queue.append((node.left,vertical-1,level+1))
+
+				if node.right:
+
+					queue.append((node.right, vertical+1,level+1))
+
+
+		result = []
+
+		for vertical in range(minVertical,maxVertical+1):
+
+			verticalResult = []
+
+			for levelNodes in sorted(verticalNodes[vertical]):
+
+				verticalResult.append(levelNodes[1])
+
+			result.append(verticalResult)
+
+		return result
+
+		
