2f18a16d5905e680e4a1910a8866d58f852e78d2
Aditya Thakur <ast.thakur@gmail.com>
2023-10-22 01:11:50 -0400

    Time: 80 ms (24.75%), Space: 16.3 MB (95.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,33 +1,22 @@
-from collections import deque
-
 class Solution:
 
 	def floodFill(self,image,startRow,startColumn,newColor):
 
-		numRows, numColumns, directions, oldColor = len(image), len(image[0]), [(-1,0),(1,0),(0,-1),(0,1)], image[startRow][startColumn]
+		numRows, numColumns, visited, directions, oldColor = len(image), len(image[0]), set(), [(-1,0),(1,0),(0,-1),(0,1)], image[startRow][startColumn]
 
-		queue = deque([(startRow,startColumn)])
-		visited = set({(startRow,startColumn)})
-
-		while queue:
-
-			currRow, currColumn = queue.popleft()
-
-			image[currRow][currColumn] = newColor
-
-			for rowDirection, columnDirection in directions:
-
-				neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
-
-				if 0 <= neighborRow < numRows and 0 <= neighborColumn < numColumns and (neighborRow,neighborColumn) not in visited and image[neighborRow][neighborColumn] == oldColor:
-
-					queue.append((neighborRow,neighborColumn))
-					visited.add((neighborRow,neighborColumn))
+		self.dfs(image,startRow,startColumn,numRows,numColumns,oldColor,newColor,directions,visited)
 
 		return image
 
-				
+	def dfs(self,image,currRow,currColumn,numRows,numColumns,oldColor,newColor,directions,visited):
 
-			
+		visited.add((currRow,currColumn))
+		image[currRow][currColumn] = newColor
 
-			
+		for rowDirection, columnDirection in directions:
+
+			neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
+
+			if 0 <= neighborRow < numRows and 0 <= neighborColumn < numColumns and (neighborRow,neighborColumn) not in visited and image[neighborRow][neighborColumn] == oldColor:
+
+				self.dfs(image,neighborRow,neighborColumn,numRows,numColumns,oldColor,newColor,directions,visited)
