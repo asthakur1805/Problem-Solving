8e5e78c8da5eaa2ba8fd7d34312caa82468b14ce
Aditya Thakur <ast.thakur@gmail.com>
2023-10-24 13:02:33 -0400

    Time: 52 ms (80.62%), Space: 16.1 MB (92.29%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,25 +2,22 @@ from collections import deque
 
 class Solution:
 
-	def orangesRotting(self, grid):
+	def orangesRotting(self,grid):
 
-		# 0: Empty, 1: Fresh, 2: Rotten
+		numRows, numColumns, numFresh, directions, time, queue = len(grid), len(grid[0]), 0, [(-1,0),(1,0),(0,-1),(0,1)], 0, deque([])
 
-		numRows, numColumns, numFresh, queue, directions = len(grid), len(grid[0]), 0, deque([]), [(0,1),(0,-1),(1,0),(-1,0)]
 
 		for currRow in range(numRows):
 
 			for currColumn in range(numColumns):
 
-				if grid[currRow][currColumn] == 2:
-
-					queue.append((currRow, currColumn))
-
-				elif grid[currRow][currColumn] == 1:
+				if grid[currRow][currColumn] == 1:
 
 					numFresh += 1
 
-		time = 0
+				elif grid[currRow][currColumn] == 2:
+
+					queue.append((currRow,currColumn))
 
 		while queue and numFresh > 0:
 
@@ -34,8 +31,8 @@ class Solution:
 
 					if 0 <= neighborRow < numRows and 0 <= neighborColumn < numColumns and grid[neighborRow][neighborColumn] == 1:
 
-						queue.append((neighborRow, neighborColumn))
-
+						queue.append((neighborRow,neighborColumn))
+	
 						grid[neighborRow][neighborColumn] = 2
 	
 						numFresh -= 1
@@ -43,7 +40,5 @@ class Solution:
 			time += 1
 
 		return time if numFresh == 0 else -1
-
-		
 
 		
