95c79585c589451bcb8161bc06ba5f758f3c0092
Aditya Thakur <ast.thakur@gmail.com>
2023-10-31 23:41:40 -0400

    Time: 60 ms (17.87%), Space: 20.3 MB (92.13%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,26 @@
+class Solution:
+
+	def kthSmallest(self, root, K):
+
+		self.K, self.result = K, None
+
+		self.inorder(root)
+
+		return self.result
+
+	def inorder(self, node):
+
+		if not node:
+
+			return
+
+		self.inorder(node.left)
+
+		self.K -= 1
+
+		if self.K == 0:
+
+			self.result = node.val
+			return
+		
+		self.inorder(node.right)
