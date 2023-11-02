3b6c5eedea7e1ecd4956de9902c31eb972ca7020
Aditya Thakur <ast.thakur@gmail.com>
2023-11-02 00:10:55 -0400

    Time: 781 ms (11.00%), Space: 22.3 MB (87.95%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,11 +1,49 @@
-from heapq import heapify, heappop
-
 class Solution:
 
 	def kClosest(self,points,K):
 
-		minHeap = [(x**2+y**2,[x,y]) for [x,y] in points]
+		if K >= len(points):
 
-		heapify(minHeap)
+			return points
 
-		return [heappop(minHeap)[1] for _ in range(K)]
+		return self.quickSelect(points,K,0,len(points)-1)
+
+	def quickSelect(self,points,K,left,pivot):
+
+		mid = left + (pivot - left) // 2
+
+		points[mid], points[pivot] = points[pivot], points[mid]
+
+		partition = left
+
+		pivotDistance = self.calculateDistance(points[pivot])
+
+		for curr in range(left,pivot):
+
+			if self.calculateDistance(points[curr]) <= pivotDistance:
+
+				points[partition], points[curr] = points[curr], points[partition]
+				partition += 1
+
+		points[partition], points[pivot] = points[pivot], points[partition]
+
+		if K == partition:
+
+			return points[:K]
+
+		return self.quickSelect(points,K,left,partition-1) if K < partition else self.quickSelect(points,K,partition+1,pivot)
+
+	def calculateDistance(self,point):
+
+		return point[0]**2 + point[1]**2
+
+
+
+			
+
+
+
+		
+		
+
+		
