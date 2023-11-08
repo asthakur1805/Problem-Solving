a95e412aac72210811e7d09b0f5e3b9df4cf4f43
Aditya Thakur <ast.thakur@gmail.com>
2023-11-08 18:15:56 -0500

    Time: 70 ms (37.35%), Space: 28.9 MB (18.66%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -2,14 +2,43 @@ class Solution:
 
 	def lowestCommonAncestor(self,root,firstNode,secondNode):
 
-		if not root: return None
-	
-		if root.val in (firstNode.val,secondNode.val): return root
+		self.firstPath, self.secondPath, builder = None, None, []
 
-		leftNode, rightNode = self.lowestCommonAncestor(root.left,firstNode,secondNode), self.lowestCommonAncestor(root.right,firstNode,secondNode)
+		self.helper(root,builder,firstNode,secondNode)
 
-		if leftNode and rightNode: return root
+		firstIndex, secondIndex = 0, 0
 
-		if not leftNode and not rightNode: return None
+		while firstIndex < len(self.firstPath) and secondIndex < len(self.secondPath) and self.firstPath[firstIndex] == self.secondPath[secondIndex]:
 
-		return leftNode if leftNode else rightNode
+			result = self.firstPath[firstIndex]
+
+			firstIndex += 1
+			secondIndex += 1
+
+		return result
+
+	def helper(self,node,builder,firstNode,secondNode):
+
+		if not node or (self.firstPath and self.secondPath):
+
+			return
+
+		builder.append(node)
+
+		if node.val == firstNode.val:
+
+			self.firstPath = builder.copy()
+
+		if node.val == secondNode.val:
+
+			self.secondPath = builder.copy()
+
+		self.helper(node.left,builder,firstNode,secondNode)
+
+		self.helper(node.right,builder,firstNode,secondNode)
+
+		builder.pop()
+
+
+
+		
