5603df75d7aa9a221d5307d99109bab636f7275f
Aditya Thakur <ast.thakur@gmail.com>
2023-11-09 12:18:38 -0500

    Time: 74 ms (63.29%), Space: 16.4 MB (50.73%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,22 +1,28 @@
+from collections import deque
+
 class Solution:
 
 	def floodFill(self,image,startRow,startColumn,newColor):
 
-		numRows, numColumns, visited, directions, oldColor = len(image), len(image[0]), set(), [(-1,0),(1,0),(0,-1),(0,1)], image[startRow][startColumn]
+		oldColor, numRows, numColumns, visited, directions = image[startRow][startColumn], len(image), len(image[0]), set({}), [(-1,0),(1,0),(0,-1),(0,1)]
 
-		self.dfs(image,startRow,startColumn,numRows,numColumns,oldColor,newColor,directions,visited)
+		queue = deque([(startRow,startColumn)])
+		visited.add((startRow,startColumn))
+
+		while queue:
+
+			currRow, currColumn = queue.popleft()
+
+			image[currRow][currColumn] = newColor
+
+			for rowDirection, columnDirection in directions:
+
+				neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
+
+				if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and image[neighborRow][neighborColumn] == oldColor and (neighborRow,neighborColumn) not in visited:
+
+					queue.append((neighborRow,neighborColumn))
+					visited.add((neighborRow,neighborColumn))
 
 		return image
-
-	def dfs(self,image,currRow,currColumn,numRows,numColumns,oldColor,newColor,directions,visited):
-
-		visited.add((currRow,currColumn))
-		image[currRow][currColumn] = newColor
-
-		for rowDirection, columnDirection in directions:
-
-			neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
-
-			if 0 <= neighborRow < numRows and 0 <= neighborColumn < numColumns and (neighborRow,neighborColumn) not in visited and image[neighborRow][neighborColumn] == oldColor:
-
-				self.dfs(image,neighborRow,neighborColumn,numRows,numColumns,oldColor,newColor,directions,visited)
+		
