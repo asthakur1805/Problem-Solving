7e69a6a8bb26324173ff464d1a2c77e956915c5f
Aditya Thakur <ast.thakur@gmail.com>
2023-11-09 01:38:51 -0500

    Time: 286 ms (63.29%), Space: 23.9 MB (39.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,37 @@
+from collections import deque
+
 class Solution:
 
 	def numIslands(self,grid):
 
-		numRows, numColumns, visited, directions, result = len(grid), len(grid[0]), set(), [(-1,0),(1,0),(0,-1),(0,1)], 0
+		numRows, numColumns, visited, result, directions = len(grid), len(grid[0]), set({}), 0, [(-1,0),(1,0),(0,-1),(0,1)]
 
 		for startRow in range(numRows):
 
 			for startColumn in range(numColumns):
 
-				if grid[startRow][startColumn] == "1" and (startRow,startColumn) not in visited:
+				if (startRow,startColumn) not in visited and grid[startRow][startColumn] == "1":
 
 					result += 1
-					
-					self.dfs(grid,startRow,startColumn,numRows,numColumns,visited,directions)
+
+					self.bfs(grid,startRow,startColumn,numRows,numColumns,visited,directions)
 
 		return result
 
-	def dfs(self,grid,currRow,currColumn,numRows,numColumns,visited,directions):
+	def bfs(self,grid,startRow,startColumn,numRows,numColumns,visited,directions):
 
-		visited.add((currRow,currColumn))
+		queue = deque([(startRow,startColumn)])
+		visited.add((startRow,startColumn))
 
-		for rowDirection, columnDirection in directions:
+		while queue:
 
-			neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
+			currRow, currColumn = queue.popleft()
 
-			if 0 <= neighborRow < numRows and 0 <= neighborColumn < numColumns and grid[neighborRow][neighborColumn] == "1" and (neighborRow,neighborColumn) not in visited:
+			for rowDirection, columnDirection in directions:
 
-				self.dfs(grid,neighborRow,neighborColumn,numRows,numColumns,visited,directions)
+				neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
 
-					
+				if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and grid[neighborRow][neighborColumn] == "1" and (neighborRow,neighborColumn) not in visited:
+
+					queue.append((neighborRow,neighborColumn))
+					visited.add((neighborRow,neighborColumn))
