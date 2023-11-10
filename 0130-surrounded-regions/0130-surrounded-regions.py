793003fac6d0e625ce63bd0091fcb7522c167ceb
Aditya Thakur <ast.thakur@gmail.com>
2023-11-10 15:24:48 -0500

    Time: 135 ms (69.48%), Space: 17.5 MB (96.13%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -2,44 +2,40 @@ from collections import deque
 
 class Solution:
 
-	def solve(self, board):
+	def solve(self,board):
 
-		numRows, numColumns, visited, queue, directions = len(board), len(board[0]), set({}), deque([]), [(0,1),(0,-1),(1,0),(-1,0)]
+		numRows, numColumns, visited, directions = len(board), len(board[0]), set({}), [(-1,0),(1,0),(0,-1),(0,1)]
+
+		queue = deque([])
 
 		for currRow in range(numRows):
 
 			for currColumn in range(numColumns):
 
-				if (currRow in (0,numRows-1) or currColumn in (0,numColumns-1)) and board[currRow][currColumn]=='O':
+				if board[currRow][currColumn] == "O" and (currRow in (0,numRows-1) or currColumn in (0,numColumns-1)):
 
-					visited.add((currRow,currColumn))
 					queue.append((currRow,currColumn))
+					visited.add((currRow,currColumn))
 
 		while queue:
 
-			for _ in range(len(queue)):
+			currRow,currColumn = queue.popleft()
 
-				currRow, currColumn = queue.popleft()
+			for rowDirection,columnDirection in directions:
 
-				for rowDirection, columnDirection in directions:
+				neighborRow,neighborColumn = currRow+rowDirection, currColumn+columnDirection
 
-					neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
+				if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and (neighborRow,neighborColumn) not in visited and board[neighborRow][neighborColumn] == 'O':
 
-					if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and (neighborRow,neighborColumn) not in visited and board[neighborRow][neighborColumn]=='O':
+					queue.append((neighborRow,neighborColumn))
+					visited.add((neighborRow,neighborColumn))
 
-						visited.add((neighborRow,neighborColumn))
-						queue.append((neighborRow,neighborColumn))
+		for currRow in range(numRows):
 
-		for currRow in range(1,numRows-1):
+			for currColumn in range(numColumns):
 
-			for currColumn in range(1, numColumns-1):
-
-				if board[currRow][currColumn] == 'O' and (currRow,currColumn) not in visited:
+				if (currRow,currColumn) not in visited:
 
 					board[currRow][currColumn] = 'X'
 
-		return board
-
-						
-
-		
+			
