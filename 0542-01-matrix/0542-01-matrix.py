0870c9a98dd5d9f5902d2ccadc2a3e7426e9255e
Aditya Thakur <ast.thakur@gmail.com>
2023-11-10 15:01:41 -0500

    Time: 563 ms (50.15%), Space: 19.6 MB (54.94%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,9 +2,9 @@ from collections import deque
 
 class Solution:
 
-	def updateMatrix(self, matrix):
+	def updateMatrix(self,matrix):
 
-		numRows, numColumns, visited, directions = len(matrix), len(matrix[0]), set({}), [(0,1),(0,-1),(1,0),(-1,0)]
+		numRows, numColumns, visited, directions = len(matrix), len(matrix[0]), set({}), [(-1,0),(1,0),(0,-1),(0,1)]
 
 		queue = deque([])
 
@@ -13,26 +13,22 @@ class Solution:
 			for currColumn in range(numColumns):
 
 				if matrix[currRow][currColumn] == 0:
-
+				
 					queue.append((currRow,currColumn))
-
 					visited.add((currRow,currColumn))
 
 		while queue:
+	
+			currRow,currColumn = queue.popleft()
 
-			for _ in range(len(queue)):
-			
-				currRow, currColumn = queue.popleft()
+			for rowDirection,columnDirection in directions:
 
-				for rowDirection, columnDirection in directions:
+				neighborRow,neighborColumn = currRow+rowDirection, currColumn+columnDirection
 
-					neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
+				if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and matrix[neighborRow][neighborColumn]==1 and (neighborRow,neighborColumn) not in visited:
 
-					if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and (neighborRow,neighborColumn) not in visited and matrix[neighborRow][neighborColumn]==1:
-
-						matrix[neighborRow][neighborColumn] = matrix[currRow][currColumn]+1
-						queue.append((neighborRow,neighborColumn))
-						visited.add((neighborRow,neighborColumn))
+					matrix[neighborRow][neighborColumn]=matrix[currRow][currColumn]+1
+					queue.append((neighborRow,neighborColumn))
+					visited.add((neighborRow,neighborColumn))
 
 		return matrix
-		
