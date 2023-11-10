9a5103b00ccb8be2529f8bc5043df9baf033dae3
Aditya Thakur <ast.thakur@gmail.com>
2023-11-10 16:32:06 -0500

    Time: 586 ms (51.41%), Space: 23.5 MB (82.61%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -2,9 +2,9 @@ from collections import deque
 
 class Solution:
 
-	def numEnclaves(self, grid):
+	def numEnclaves(self,grid):
 
-		numRows, numColumns, visited, directions, totalLand, boundaryLand = len(grid), len(grid[0]), set({}), [(0,1),(0,-1),(1,0),(-1,0)], 0, 0
+		numRows, numColumns, visited, directions, totalLand = len(grid), len(grid[0]), set({}), [(-1,0),(1,0),(0,-1),(0,1)], 0
 
 		queue = deque([])
 
@@ -13,29 +13,25 @@ class Solution:
 			for currColumn in range(numColumns):
 
 				totalLand += grid[currRow][currColumn]
-	
+
 				if grid[currRow][currColumn] == 1 and (currRow in (0,numRows-1) or currColumn in (0,numColumns-1)):
 
-					boundaryLand += 1
-					visited.add((currRow,currColumn))
 					queue.append((currRow,currColumn))
+					visited.add((currRow,currColumn))
+
 
 		while queue:
 
-			for _ in range(len(queue)):
+			currRow, currColumn = queue.popleft()
 
-				currRow, currColumn = queue.popleft()
+			for rowDirection, columnDirection in directions:
 
-				for rowDirection, columnDirection in directions:
+				neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection 
 
-					neighborRow, neighborColumn = currRow+rowDirection, currColumn+columnDirection
+				if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and (neighborRow,neighborColumn) not in visited and grid[neighborRow][neighborColumn] == 1:
 
-					if 0<=neighborRow<numRows and 0<=neighborColumn<numColumns and (neighborRow,neighborColumn) not in visited and grid[neighborRow][neighborColumn]==1:
+					queue.append((neighborRow,neighborColumn))
+					visited.add((neighborRow,neighborColumn))
 
-						visited.add((neighborRow,neighborColumn))
-						queue.append((neighborRow,neighborColumn))
-						boundaryLand += 1
-
-		return totalLand-boundaryLand
-			
-					
+		return totalLand - len(visited)
+		
