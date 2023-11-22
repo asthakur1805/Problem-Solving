d30116821eff839956057b4c725304c44f89343d
Aditya Thakur <ast.thakur@gmail.com>
2023-11-21 23:44:04 -0500

    Time: 39 ms (69.63%), Space: 17.1 MB (42.34%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -0,0 +1,27 @@
+class Solution:
+
+	def sumOfLeftLeaves(self,root):
+
+		self.resultSum = 0
+
+		self.preorder(root,False)
+
+		return self.resultSum
+
+	def preorder(self,node,isLeftNode):
+
+		if not node:
+
+			return
+
+		if not node.left and not node.right:
+
+			if isLeftNode:
+
+				self.resultSum += node.val
+
+			return
+
+		self.preorder(node.left,True)
+
+		self.preorder(node.right,False)
