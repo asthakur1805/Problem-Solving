33c9e4713526b2bf9093a1de6bcbdc3edb4b14e1
Aditya Thakur <ast.thakur@gmail.com>
2023-11-23 13:19:47 -0500

    Time: 157 ms (52.14%), Space: 33.7 MB (66.89%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,56 @@
+class TrieNode:
+
+	def __init__(self):
+
+		self.children = {}
+
+		self.endOfWord = False
+
+class Trie:
+
+	def __init__(self):
+
+		self.root = TrieNode()
+
+	def insert(self,word):
+
+		curr = self.root
+
+		for char in word:
+
+			if char not in curr.children:
+
+				curr.children[char] = TrieNode()
+
+			curr = curr.children[char]
+
+		curr.endOfWord = True
+
+	def search(self,word):
+
+		curr = self.root
+
+		for char in word:
+
+			if char not in curr.children:
+
+				return False
+
+			curr = curr.children[char]
+
+		return curr.endOfWord
+
+	def startsWith(self,prefix):
+	
+		curr = self.root
+
+		for char in prefix:
+
+			if char not in curr.children:
+		
+				return False
+
+			curr = curr.children[char]
+
+		return True
+
