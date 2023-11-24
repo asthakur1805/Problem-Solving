afbd5e51acf485141200cfa13f836d6856d25f00
Aditya Thakur <ast.thakur@gmail.com>
2023-11-24 13:39:28 -0500

    Time: 48 ms (75.81%), Space: 16.3 MB (58.70%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+
+	def totalNQueens(self,N):
+
+		colSet, posDiagSet, negDiagSet = set(), set(), set()
+
+		self.resultCount = 0
+
+		self.helper(0,N,colSet,posDiagSet,negDiagSet)
+
+		return self.resultCount
+
+	def helper(self,row,N,colSet,posDiagSet,negDiagSet):
+
+		if row == N:
+
+			self.resultCount += 1
+
+			return
+
+		for column in range(N):
+
+			if column in colSet or row+column in posDiagSet or row-column in negDiagSet:
+
+				continue
+
+			colSet.add(column)
+			posDiagSet.add(row+column)
+			negDiagSet.add(row-column)
+
+			self.helper(row+1,N,colSet,posDiagSet,negDiagSet)
+
+			colSet.remove(column)
+			posDiagSet.remove(row+column)
+			negDiagSet.remove(row-column)
+
+	
+		
