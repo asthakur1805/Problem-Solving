2c07b02793ede58a212e57455a450f849c2d4b22
Aditya Thakur <ast.thakur@gmail.com>
2023-11-24 13:17:28 -0500

    Time: 53 ms (79.61%), Space: 16.9 MB (26.36%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,43 @@
+class Solution:
+
+	def solveNQueens(self,N):
+
+		colSet, posDiagSet, negDiagSet = set(), set(), set()
+
+		board = [['.' for _ in range(N)] for _ in range(N)]
+
+		result = []
+
+		self.helper(board,0,N,colSet,posDiagSet,negDiagSet,result)
+
+		return result
+
+	def helper(self,board,row,N,colSet,posDiagSet,negDiagSet,result):
+
+		if row == N:
+
+			deepCopy = [''.join(row) for row in board]
+			
+			result.append(deepCopy)
+
+			return
+
+		for column in range(N):
+
+			if column in colSet or row+column in posDiagSet or row-column in negDiagSet:
+
+				continue
+
+			colSet.add(column)
+			posDiagSet.add(row+column)
+			negDiagSet.add(row-column)
+			board[row][column] = 'Q'
+
+			self.helper(board,row+1,N,colSet,posDiagSet,negDiagSet,result)
+
+			colSet.remove(column)
+			posDiagSet.remove(row+column)
+			negDiagSet.remove(row-column)
+			board[row][column] = '.'
+
+		
