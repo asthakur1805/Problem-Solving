087452c6db1fbcdf4b8007d35921758a5e4fc8ee
Aditya Thakur <ast.thakur@gmail.com>
2023-11-28 15:54:31 -0500

    Time: 43 ms (69.80%), Space: 16.8 MB (9.52%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,27 +1,35 @@
+from collections import deque
+
 class Solution:
 
 	def cloneGraph(self,node):
 
 		if not node:
 
-			return
-
-		oldToNew = {}
-
-		return self.clone(node,oldToNew)
-
-	def clone(self,node,oldToNew):
-
-		if node in oldToNew:
-
-			return oldToNew[node]
+			return 
 
 		copy = Node(node.val)
 
-		oldToNew[node] = copy
+		oldToNew = {node:copy}
 
-		for neighborNode in node.neighbors:
+		queue = deque([node])
 
-			copy.neighbors.append(self.clone(neighborNode,oldToNew))
+		while queue:
+
+			for _ in range(len(queue)):
+
+				oldNode = queue.popleft()
+
+				for neighborNode in oldNode.neighbors:
+
+					if neighborNode not in oldToNew:
+
+						neighborCopy = Node(neighborNode.val)
+
+						oldToNew[neighborNode] = neighborCopy
+
+						queue.append(neighborNode)
+
+					oldToNew[oldNode].neighbors.append(oldToNew[neighborNode])
 
 		return copy
