679fb561c16cd065c0d7a92911abc8b029579c45
Aditya Thakur <ast.thakur@gmail.com>
2023-11-29 16:48:06 -0500

    Time: 70 ms (76.98%), Space: 23.9 MB (10.74%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -2,20 +2,31 @@ class Solution:
 
 	def countNodes(self,root):
 
-		self.count = 0
-
-		self.preorder(root)
-
-		return self.count
-
-	def preorder(self,node):
-
-		if not node:
+		if not root:
 
 			return 0
 
-		self.count += 1
+		leftDepth, rightDepth = 0, 0
 
-		self.preorder(node.left)
+		curr = root
 
-		self.preorder(node.right)
+		while curr:
+
+			leftDepth += 1
+
+			curr = curr.left
+
+		curr = root
+
+		while curr:
+
+			rightDepth += 1
+
+			curr = curr.right
+
+		return (1 << leftDepth)-1 if leftDepth == rightDepth else 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+
+		
+
+			
