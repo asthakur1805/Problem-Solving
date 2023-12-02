e3141dfc86942970161b72bae7b2754729feea3c
Aditya Thakur <ast.thakur@gmail.com>
2023-12-02 16:08:30 -0500

    Time: 110 ms (49.89%), Space: 16.5 MB (19.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,13 +2,14 @@ class Solution:
 
 	def characterReplacement(self,inputStr,maxReplacements):
 
-		start, counts, result = 0, [0]*26, 0
+		start, counts, result, maxCount = 0, [0]*26, 0, 0
 
 		for end in range(len(inputStr)):
 
 			counts[ord(inputStr[end])-ord('A')] += 1
+			maxCount = max(maxCount,counts[ord(inputStr[end])-ord('A')])
 
-			while (end-start+1) - max(counts) > maxReplacements:
+			while (end-start+1) - maxCount > maxReplacements:
 
 				counts[ord(inputStr[start])-ord('A')] -= 1
 				start += 1
