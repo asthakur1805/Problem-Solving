22e5c46349a40e1e73eda56e0bc00ae658eed60c
Aditya Thakur <ast.thakur@gmail.com>
2023-12-04 20:31:58 -0500

    Time: 62 ms (51.07%), Space: 16.4 MB (67.87%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -2,24 +2,10 @@ class Solution:
 
 	def minCostClimbingStairs(self,cost):
 
-		targetStep = len(cost)
+		first, second = cost[0], cost[1]
 
-		return min(self.calculateMinCost(cost,targetStep-1,{}),self.calculateMinCost(cost,targetStep-2,{}))
+		for index in range(2,len(cost)):
 
-	def calculateMinCost(self,cost,currStep,cache):
+			first, second = second, cost[index] + min(first,second)
 
-		if currStep <= 1:
-
-			return cost[currStep]
-
-		if currStep in cache:
-
-			return cache[currStep]
-
-		cache[currStep] = cost[currStep] + min(self.calculateMinCost(cost,currStep-1,cache),self.calculateMinCost(cost,currStep-2,cache))
-
-		return cache[currStep]
-
-
-
-		
+		return min(first,second)
