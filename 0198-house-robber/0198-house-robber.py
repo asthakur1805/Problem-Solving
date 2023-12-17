52f48c766ebd7b311c68d52327218c79def3e852
Aditya Thakur <ast.thakur@gmail.com>
2023-12-16 23:06:10 -0500

    Time: 38 ms (68.58%), Space: 16.3 MB (59.25%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,23 @@
+class Solution:
+
+	def rob(self,nums):
+
+		return self.helper(nums,len(nums)-1,{})
+
+	def helper(self,nums,index,cache):
+
+		if index == 0: return nums[index]
+
+		if index < 0: return 0
+
+		if index in cache:
+
+			return cache[index]
+
+		rob = nums[index] + self.helper(nums,index-2,cache)
+
+		skip = self.helper(nums,index-1,cache)
+
+		cache[index] = max(rob,skip)
+
+		return cache[index]
