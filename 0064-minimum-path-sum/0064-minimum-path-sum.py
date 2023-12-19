17764d885c066689a0cdd1e8c7bdc1ed24c6ae34
Aditya Thakur <ast.thakur@gmail.com>
2023-12-19 14:52:31 -0500

    Time: 130 ms (5.67%), Space: 22.2 MB (9.06%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+
+	def minPathSum(self,grid):
+
+		return self.helper(grid,0,0,len(grid),len(grid[0]),{})
+
+	def helper(self,grid,currRow,currColumn,numRows,numColumns,cache):
+
+		if currRow >= numRows or currColumn >= numColumns:
+
+			return float('inf')
+
+		if (currRow,currColumn) == (numRows-1,numColumns-1):
+
+			return grid[currRow][currColumn]
+
+		if (currRow,currColumn) in cache:
+
+			return cache[(currRow,currColumn)]
+
+		right = self.helper(grid,currRow,currColumn+1,numRows,numColumns,cache)
+		down = self.helper(grid,currRow+1,currColumn,numRows,numColumns,cache)
+
+		cache[(currRow,currColumn)] = grid[currRow][currColumn] + min(right,down)
+
+		return cache[(currRow,currColumn)]
