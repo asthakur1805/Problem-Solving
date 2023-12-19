ba986d9d7dce008763415ae8685e40892d261348
Aditya Thakur <ast.thakur@gmail.com>
2023-12-18 19:16:45 -0500

    Time: 8232 ms (5.00%), Space: 17.6 MB (20.31%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,33 @@
+class Solution:
+
+	def canJump(self,nums):
+
+		dp = [False] * len(nums)
+
+		dp[-1] = True
+
+		for index in range(len(nums)-2,-1,-1):
+		
+			for jump in range(1,nums[index]+1):
+
+				if index+jump<len(nums) and dp[index+jump]:
+
+					dp[index] = True
+					break
+				
+		return dp[0]
+
+		
+		
+		
+
+		
+
+		
+		
+		
+
+		
+		
+
+		
