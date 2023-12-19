6073e752a60d53ebab13d5d85849a1d598dd67ca
Aditya Thakur <ast.thakur@gmail.com>
2023-12-19 15:07:44 -0500

    Time: 113 ms (13.90%), Space: 16.9 MB (95.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,23 +4,29 @@ class Solution:
 
 		numRows, numColumns = len(grid), len(grid[0])
 
-		dp = [[0]*numColumns for _ in range(numRows)]
+		prev = [0]*numColumns
 
 		for currRow in range(numRows-1,-1,-1):
+
+			curr = [0]*numColumns 
 
 			for currColumn in range(numColumns-1,-1,-1):
 
 				if (currRow,currColumn) == (numRows-1,numColumns-1):
 
-					dp[currRow][currColumn] = grid[currRow][currColumn]
+					curr[currColumn] = grid[currRow][currColumn]
 
 				else:
 					
-					right = dp[currRow][currColumn+1] if currColumn < numColumns-1 else float('inf')
-					down = dp[currRow+1][currColumn] if currRow < numRows-1 else float('inf')
+					right = curr[currColumn+1] if currColumn < numColumns-1 else float('inf')
+					down = prev[currColumn] if currRow < numRows-1 else float('inf')
 
-					dp[currRow][currColumn] = grid[currRow][currColumn] + min(right,down)
+					curr[currColumn] = grid[currRow][currColumn] + min(right,down)
+			
+			prev = curr
 
-		return dp[0][0]
+		return prev[0]
+
+					
 
 					
