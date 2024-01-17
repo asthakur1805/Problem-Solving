14d70b3e688d4deab03dfcd3ae847265bf1e58ae
Aditya Thakur <ast.thakur@gmail.com>
2024-01-17 00:53:24 -0500

    Time: 520 ms (48.70%), Space: 17.6 MB (95.61%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.py
+++ 0741-cherry-pickup/0741-cherry-pickup.py
@@ -4,9 +4,11 @@ class Solution:
 
 		order = len(grid)
 
-		dp = [[[float('-inf')]*order for _ in range(order)] for _ in range(order)]
+		prev = [[float('-inf')]*order for _ in range(order)]
 
 		for firstRow in range(order-1,-1,-1):
+
+			dp = [[float('-inf')]*order for _ in range(order)] 
 
 			for firstColumn in range(order-1,-1,-1):
 
@@ -17,27 +19,23 @@ class Solution:
 					# VVVVIMP - TRACE 2x2 GRID WITH ALL 1's TO UNDERSTAND
 					if 0 <= secondColumn < order and grid[firstRow][firstColumn] != -1 and grid[secondRow][secondColumn] != -1:
 
-						if (firstRow,firstColumn) == (order-1,order-1):
-
-							dp[firstRow][firstColumn][secondRow] = grid[firstRow][firstColumn]
-
-						else:
-
 							if (firstRow,firstColumn) == (secondRow,secondColumn):
 
-								dp[firstRow][firstColumn][secondRow] = grid[firstRow][firstColumn]
+								dp[firstColumn][secondRow] = grid[firstRow][firstColumn]
 
 							else:
 
-								dp[firstRow][firstColumn][secondRow] = grid[firstRow][firstColumn] + grid[secondRow][secondColumn]
+								dp[firstColumn][secondRow] = grid[firstRow][firstColumn] + grid[secondRow][secondColumn]
 
 							if (firstRow,firstColumn) != (order-1,order-1):
 
-								bothRight = dp[firstRow][firstColumn+1][secondRow] if firstColumn+1<order else float('-inf')
-								rightDown = dp[firstRow][firstColumn+1][secondRow+1] if firstColumn+1<order and secondRow+1<order else float('-inf')
-								downRight = dp[firstRow+1][firstColumn][secondRow] if firstRow+1<order else float('-inf') 
-								bothDown = dp[firstRow+1][firstColumn][secondRow+1] if firstRow+1<order and secondRow+1<order else float('-inf')
+								bothRight = dp[firstColumn+1][secondRow] if firstColumn+1<order else float('-inf')
+								rightDown = dp[firstColumn+1][secondRow+1] if firstColumn+1<order and secondRow+1<order else float('-inf')
+								downRight = prev[firstColumn][secondRow]  
+								bothDown = prev[firstColumn][secondRow+1] if secondRow+1<order else float('-inf')
 
-								dp[firstRow][firstColumn][secondRow] += max(bothRight,rightDown,downRight,bothDown)
+								dp[firstColumn][secondRow] += max(bothRight,rightDown,downRight,bothDown)
 
-		return max(0,dp[0][0][0])
+			prev = dp
+
+		return max(0,prev[0][0])
