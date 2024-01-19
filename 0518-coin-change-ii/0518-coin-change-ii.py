ae972c5b60766a9c5af7e2d7ab8798cbd5bbc9f4
Aditya Thakur <ast.thakur@gmail.com>
2024-01-19 16:41:10 -0500

    Time: 166 ms (67.37%), Space: 17 MB (79.83%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,19 +2,25 @@ class Solution:
 
 	def change(self,amount,coins):
 
-		dp = [[0]*(amount+1) for _ in range(len(coins))]
+		prev = [0]*(amount+1)
 
 		for currAmount in range(0,amount+1,coins[0]):
 
-			dp[0][currAmount] = 1
+			prev[currAmount] = 1
 
 		for index in range(1,len(coins)):
 
+			dp = [0]*(amount+1)
+
 			for currAmount in range(amount+1):
 
-				pick = dp[index][currAmount-coins[index]] if currAmount >= coins[index] else 0
-				notPick = dp[index-1][currAmount]
+				pick = dp[currAmount-coins[index]] if currAmount >= coins[index] else 0
+				notPick = prev[currAmount]
 
-				dp[index][currAmount] = pick+notPick
+				dp[currAmount] = pick+notPick
+		
+			prev = dp
 
-		return dp[len(coins)-1][amount]
+
+		return prev[amount]
+				
