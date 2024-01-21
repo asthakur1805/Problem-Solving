0c56b16b9614233d5fecbe470c798681df3b3ae7
Aditya Thakur <ast.thakur@gmail.com>
2024-01-20 23:40:05 -0500

    Time: 3241 ms (5.03%), Space: 18.4 MB (61.42%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+
+	def nextGreaterElements(self,nums):
+
+		result = [-1] * len(nums)
+
+		for currIndex in range(len(nums)):
+
+			for offset in range(1,len(nums)):
+
+				nextIndex = (currIndex + offset) % len(nums)
+
+				if nums[nextIndex] > nums[currIndex]:
+
+					result[currIndex] = nums[nextIndex]
+					break
+
+		return result
