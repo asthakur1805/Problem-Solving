80f60605d59f568fbf96b081d489d2fef984489f
Aditya Thakur <ast.thakur@gmail.com>
2024-01-21 23:06:48 -0500

    Time: 1227 ms (16.88%), Space: 184.1 MB (19.34%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,11 +2,11 @@ class Solution:
 
 	def longestCommonSubsequence(self,firstStr,secondStr):
 
-		return self.helper(firstStr,secondStr,len(firstStr)-1,len(secondStr)-1,{})
+		return self.helper(firstStr,secondStr,len(firstStr),len(secondStr),{})
 
 	def helper(self,firstStr,secondStr,firstIndex,secondIndex,cache):
 
-		if firstIndex < 0 or secondIndex < 0:
+		if firstIndex == 0 or secondIndex == 0:
 
 			return 0
 
@@ -14,13 +14,14 @@ class Solution:
 
 			return cache[(firstIndex,secondIndex)]
 
-		if firstStr[firstIndex] == secondStr[secondIndex]:
+		if firstStr[firstIndex-1] == secondStr[secondIndex-1]:
 
 			cache[(firstIndex,secondIndex)] = 1 + self.helper(firstStr,secondStr,firstIndex-1,secondIndex-1,cache)
 			return cache[(firstIndex,secondIndex)]
 
 		cache[(firstIndex,secondIndex)] = max(self.helper(firstStr,secondStr,firstIndex-1,secondIndex,cache),self.helper(firstStr,secondStr,firstIndex,secondIndex-1,cache))
 		return cache[(firstIndex,secondIndex)]
+
 
 		
 		
