910e5747fc41386cb2e28f311e893e2ed4c0f96c
Aditya Thakur <ast.thakur@gmail.com>
2024-01-25 17:07:25 -0500

    Time: 35 ms (74.31%), Space: 16.6 MB (86.63%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -2,27 +2,23 @@ class Solution:
 
 	def numDecodings(self,inputStr):
 
-		dp = [0]*(len(inputStr)+1)
-
-		dp[len(inputStr)] = 1
+		first, second = 1, 0
 
 		for start in range(len(inputStr)-1,-1,-1):
 
 			if inputStr[start] == '0':
 
-				dp[start] = 0
+				second = first
+				first = 0
 
 			else:
 
-				singlePartition = dp[start+1]
+				singlePartition = first
 
-				doublePartition = dp[start+2] if start+1<len(inputStr) and (inputStr[start] == '1' or inputStr[start] == '2' and '0' <= inputStr[start+1] <= '6') else 0
+				doublePartition = second if start+1<len(inputStr) and (inputStr[start] == '1' or inputStr[start] == '2' and '0' <= inputStr[start+1] <= '6') else 0
 
-				dp[start] = singlePartition+doublePartition
+				second = first
+				first = singlePartition+doublePartition
 
-		return dp[0]
+		return first
 
-
-
-		
-		
