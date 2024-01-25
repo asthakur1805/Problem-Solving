b142205a6514aebbef0f2a6c3195ad6fb48d7d13
Aditya Thakur <ast.thakur@gmail.com>
2024-01-25 13:34:23 -0500

    Time: 564 ms (75.30%), Space: 16.5 MB (96.39%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+
+	def minInsertions(self,inputStr):
+
+		return len(inputStr) - self.longestCommonSubsequence(inputStr,inputStr[::-1])
+
+	def longestCommonSubsequence(self,firstStr,secondStr):
+
+		prev = [0]*(len(secondStr)+1) 
+
+		for firstIndex in range(1,len(firstStr)+1):
+
+			dp = [0]*(len(secondStr)+1)
+
+			for secondIndex in range(1,len(secondStr)+1):
+
+				if firstStr[firstIndex-1] == secondStr[secondIndex-1]:
+
+					dp[secondIndex] = 1 + prev[secondIndex-1]
+
+				else:
+					
+					dp[secondIndex] = max(prev[secondIndex],dp[secondIndex-1])
+		
+			prev = dp
+
+		return prev[len(secondStr)]
