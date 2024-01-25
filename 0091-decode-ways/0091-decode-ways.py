8e3f23294045d5e4452c701f3d122398a240a591
Aditya Thakur <ast.thakur@gmail.com>
2024-01-25 16:59:36 -0500

    Time: 35 ms (74.31%), Space: 16.6 MB (88.89%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -2,22 +2,27 @@ class Solution:
 
 	def numDecodings(self,inputStr):
 
-		return self.helper(inputStr,0,{})
+		dp = [0]*(len(inputStr)+1)
 
-	def helper(self,inputStr,start,cache):
+		dp[len(inputStr)] = 1
 
-		if start == len(inputStr): return 1
+		for start in range(len(inputStr)-1,-1,-1):
 
-		if inputStr[start] == '0': return 0
+			if inputStr[start] == '0':
 
-		if start in cache: return cache[start]
+				dp[start] = 0
 
-		singlePartition = self.helper(inputStr,start+1,cache)
+			else:
 
-		doublePartition = self.helper(inputStr,start+2,cache) if start+1<len(inputStr) and (inputStr[start] == '1' or inputStr[start] == '2' and '0' <= inputStr[start+1] <= '6') else 0
+				singlePartition = dp[start+1]
 
-		cache[start] = singlePartition+doublePartition
-		return cache[start]
+				doublePartition = dp[start+2] if start+1<len(inputStr) and (inputStr[start] == '1' or inputStr[start] == '2' and '0' <= inputStr[start+1] <= '6') else 0
+
+				dp[start] = singlePartition+doublePartition
+
+		return dp[0]
+
+
 
 		
 		
