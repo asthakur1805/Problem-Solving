f8eaa2f46bf743308792a31a468757bad8a93cf6
Aditya Thakur <ast.thakur@gmail.com>
2024-01-25 10:53:35 -0500

    Time: 38 ms (82.43%), Space: 17.6 MB (18.66%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,18 +1,53 @@
+class TrieNode:
+
+	def __init__(self):
+
+		self.children = {}
+		self.endOfWord = False
+
+	def add(self,word):
+
+		curr = self
+
+		for char in word:
+
+			if char not in curr.children:
+
+				curr.children[char] = TrieNode()
+
+			curr = curr.children[char]
+
+		curr.endOfWord = True
+
 class Solution:
 
 	def wordBreak(self,inputStr,wordDict):
+
+		root = TrieNode()
+		
+		for word in wordDict:
+
+			root.add(word)
 
 		dp = [False]*(len(inputStr)+1)
 
 		dp[len(inputStr)] = True
 
-		wordSet = set(wordDict)
-
 		for start in range(len(inputStr)-1,-1,-1):
+
+			curr = root
 
 			for end in range(start,len(inputStr)):
 
-				if inputStr[start:end+1] in wordSet and dp[end+1]:
+				char = inputStr[end]
+
+				if char not in curr.children:
+
+					break
+
+				curr = curr.children[char]
+
+				if curr.endOfWord and dp[end+1]:
 
 					dp[start] = True
 
