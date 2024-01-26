2d76df0a41a690b1aef5e363ebb524938330b130
Aditya Thakur <ast.thakur@gmail.com>
2024-01-26 03:06:49 -0500

    Time: 97 ms (77.84%), Space: 16.6 MB (96.76%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,30 +2,31 @@ class Solution:
 
 	def minDistance(self,firstStr,secondStr):
 
-		dp = [[0]*(len(secondStr)+1) for _ in range(len(firstStr)+1)]
+		prev = [0]*(len(secondStr)+1)
 
-		for firstIndex in range(len(firstStr)+1):
-
-			dp[firstIndex][0] = firstIndex
-		
 		for secondIndex in range(len(secondStr)+1):
 
-			dp[0][secondIndex] = secondIndex
+			prev[secondIndex] = secondIndex
 
 		for firstIndex in range(1,len(firstStr)+1):
+
+			dp = [0]*(len(secondStr)+1)
+			dp[0] = firstIndex
 
 			for secondIndex in range(1,len(secondStr)+1):
 
 				if firstStr[firstIndex-1] == secondStr[secondIndex-1]: 
 
-					dp[firstIndex][secondIndex] = dp[firstIndex-1][secondIndex-1]
+					dp[secondIndex] = prev[secondIndex-1]
 
 				else:
 
-					insertion = dp[firstIndex][secondIndex-1]
-					deletion = dp[firstIndex-1][secondIndex]
-					replacement = dp[firstIndex-1][secondIndex-1]
+					insertion = dp[secondIndex-1]
+					deletion = prev[secondIndex]
+					replacement = prev[secondIndex-1]
 
-					dp[firstIndex][secondIndex] = 1 + min(insertion,deletion,replacement)
+					dp[secondIndex] = 1 + min(insertion,deletion,replacement)
+			
+			prev = dp
 
-		return dp[len(firstStr)][len(secondStr)]
+		return prev[len(secondStr)]
