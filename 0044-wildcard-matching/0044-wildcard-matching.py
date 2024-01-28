5429d219aac3446bc5cca48d444812b16943d2b1
Aditya Thakur <ast.thakur@gmail.com>
2024-01-28 17:38:52 -0500

    Time: 377 ms (62.43%), Space: 16.5 MB (92.04%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -2,9 +2,8 @@ class Solution:
 
 	def isMatch(self,firstStr,secondStr):
 
-		dp = [[False]*(len(secondStr)+1) for _ in range(len(firstStr)+1)]
-
-		dp[0][0] = True
+		prev = [False]*(len(secondStr)+1)
+		prev[0] = True
 
 		for secondIndex in range(1,len(secondStr)+1):
 
@@ -12,31 +11,32 @@ class Solution:
 
 				if secondStr[currIndex-1] != '*':
 
-					dp[0][secondIndex] = False
+					prev[secondIndex] = False
 					break
 
 			else:
 
-				dp[0][secondIndex] = True
+				prev[secondIndex] = True
 
 		for firstIndex in range(1,len(firstStr)+1):
 
-			dp[firstIndex][0] = False
-
-		for firstIndex in range(1,len(firstStr)+1):
+			dp = [False]*(len(secondStr)+1)
 
 			for secondIndex in range(1,len(secondStr)+1):
 
 				if firstStr[firstIndex-1] == secondStr[secondIndex-1] or secondStr[secondIndex-1] == '?':
 
-					dp[firstIndex][secondIndex] = dp[firstIndex-1][secondIndex-1]
+					dp[secondIndex] = prev[secondIndex-1]
 
 				elif secondStr[secondIndex-1] == '*':
 
-					dp[firstIndex][secondIndex] = dp[firstIndex][secondIndex-1] or dp[firstIndex-1][secondIndex]
+					dp[secondIndex] = dp[secondIndex-1] or prev[secondIndex]
 					
 				else:
 
-					dp[firstIndex][secondIndex] = False
+					dp[secondIndex] = False
 
-		return dp[len(firstStr)][len(secondStr)]
+			prev = dp
+
+		return prev[len(secondStr)]
+				
