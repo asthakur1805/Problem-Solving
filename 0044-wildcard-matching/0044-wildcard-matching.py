a857e0b6e1359a8b741cd4e4a6af520bb4fa2d0f
Aditya Thakur <ast.thakur@gmail.com>
2024-01-28 16:56:30 -0500

    Time: 399 ms (59.48%), Space: 94.9 MB (25.47%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -2,23 +2,23 @@ class Solution:
 
 	def isMatch(self,firstStr,secondStr):
 
-		return self.helper(firstStr,secondStr,len(firstStr)-1,len(secondStr)-1,{})
+		return self.helper(firstStr,secondStr,len(firstStr),len(secondStr),{})
 
 	def helper(self,firstStr,secondStr,firstIndex,secondIndex,cache):
 
-		if firstIndex < 0 and secondIndex < 0:
+		if firstIndex == 0 and secondIndex == 0:
 
 			return True
 
-		if secondIndex < 0:
+		if secondIndex == 0:
 
 			return False
 
-		if firstIndex < 0:
+		if firstIndex == 0:
 
-			for currIndex in range(secondIndex+1):
+			for currIndex in range(1,secondIndex+1):
 
-				if secondStr[currIndex] != '*':
+				if secondStr[currIndex-1] != '*':
 
 					return False
 
@@ -28,19 +28,17 @@ class Solution:
 
 			return cache[(firstIndex,secondIndex)]
 
-		if firstStr[firstIndex] == secondStr[secondIndex] or secondStr[secondIndex] == '?':
+		if firstStr[firstIndex-1] == secondStr[secondIndex-1] or secondStr[secondIndex-1] == '?':
 
 			cache[(firstIndex,secondIndex)] = self.helper(firstStr,secondStr,firstIndex-1,secondIndex-1,cache)
 			return cache[(firstIndex,secondIndex)]
 
-		if secondStr[secondIndex] == '*':
+		if secondStr[secondIndex-1] == '*':
 
 			cache[(firstIndex,secondIndex)] = self.helper(firstStr,secondStr,firstIndex,secondIndex-1,cache) or self.helper(firstStr,secondStr,firstIndex-1,secondIndex,cache)
 			return cache[(firstIndex,secondIndex)]
 
 		cache[(firstIndex,secondIndex)] = False
 		return cache[(firstIndex,secondIndex)]
-
-			
 
 			
