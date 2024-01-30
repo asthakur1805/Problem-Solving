2b3ceb29262391c8b262877d1678b069cdc3e513
Aditya Thakur <ast.thakur@gmail.com>
2024-01-30 18:23:22 -0500

    Time: 500 ms (96.35%), Space: 19.2 MB (97.33%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -4,19 +4,23 @@ class Solution:
 
 		self.maxSide = 0
 
-		dp = [[0]*(len(matrix[0])+1) for _ in range(len(matrix)+1)]
+		prev = [0]*(len(matrix[0])+1)
 
 		for currRow in range(1,len(matrix)+1):
+			
+			dp = [0]*(len(matrix[0])+1)
 
 			for currColumn in range(1,len(matrix[0])+1):
 
-				left = dp[currRow][currColumn-1]
-				top = dp[currRow-1][currColumn]
-				diagonal = dp[currRow-1][currColumn-1]
+				left = dp[currColumn-1]
+				top = prev[currColumn]
+				diagonal = prev[currColumn-1]
 
 				if matrix[currRow-1][currColumn-1] == '1':
 
-					dp[currRow][currColumn] = 1+min(left,top,diagonal)
-					self.maxSide = max(self.maxSide,dp[currRow][currColumn])
+					dp[currColumn] = 1+min(left,top,diagonal)
+					self.maxSide = max(self.maxSide,dp[currColumn])
+
+			prev = dp
 					
 		return self.maxSide ** 2
