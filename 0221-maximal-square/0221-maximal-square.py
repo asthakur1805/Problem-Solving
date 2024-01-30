b1a02905005d9e0cad4b492056df63ba798baaf0
Aditya Thakur <ast.thakur@gmail.com>
2024-01-30 18:21:42 -0500

    Time: 520 ms (81.94%), Space: 19.4 MB (81.18%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -4,29 +4,19 @@ class Solution:
 
 		self.maxSide = 0
 
-		self.helper(matrix,len(matrix)-1,len(matrix[0])-1,{})
+		dp = [[0]*(len(matrix[0])+1) for _ in range(len(matrix)+1)]
 
+		for currRow in range(1,len(matrix)+1):
+
+			for currColumn in range(1,len(matrix[0])+1):
+
+				left = dp[currRow][currColumn-1]
+				top = dp[currRow-1][currColumn]
+				diagonal = dp[currRow-1][currColumn-1]
+
+				if matrix[currRow-1][currColumn-1] == '1':
+
+					dp[currRow][currColumn] = 1+min(left,top,diagonal)
+					self.maxSide = max(self.maxSide,dp[currRow][currColumn])
+					
 		return self.maxSide ** 2
-
-	def helper(self,matrix,currRow,currColumn,cache):
-
-		if currRow < 0 or currColumn < 0: 
-
-			return 0
-
-		if (currRow,currColumn) in cache:
-
-			return cache[(currRow,currColumn)]
-
-		left = self.helper(matrix,currRow,currColumn-1,cache)
-		top = self.helper(matrix,currRow-1,currColumn,cache)
-		diagonal = self.helper(matrix,currRow-1,currColumn-1,cache)
-
-		if matrix[currRow][currColumn] == '1':
-
-			cache[(currRow,currColumn)] = 1+min(left,top,diagonal)
-			self.maxSide = max(self.maxSide,cache[(currRow,currColumn)])
-			return cache[(currRow,currColumn)]
-
-		cache[(currRow,currColumn)] = 0
-		return 0
