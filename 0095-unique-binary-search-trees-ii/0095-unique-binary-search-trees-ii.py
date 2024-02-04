bf411bef72665bc684af2753720fb475105c9611
Aditya Thakur <ast.thakur@gmail.com>
2024-02-04 14:27:54 -0500

    Time: 41 ms (93.65%), Space: 17 MB (99.96%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -2,9 +2,9 @@ class Solution:
 
 	def generateTrees(self,upperBound):
 
-		return self.buildTrees(1,upperBound)
+		return self.buildTrees(1,upperBound,{})
 
-	def buildTrees(self,left,right):
+	def buildTrees(self,left,right,cache):
 
 		if left > right:
 
@@ -12,12 +12,17 @@ class Solution:
 
 		result = []
 
+		if (left,right) in cache:
+
+			return cache[(left,right)]
+
 		for nodeVal in range(left,right+1):
 
-			for leftSubTree in self.buildTrees(left,nodeVal-1):
+			for leftSubTree in self.buildTrees(left,nodeVal-1,cache):
 
-				for rightSubTree in self.buildTrees(nodeVal+1,right):
+				for rightSubTree in self.buildTrees(nodeVal+1,right,cache):
 
 					result.append(TreeNode(nodeVal,leftSubTree,rightSubTree))
 
+		cache[(left,right)] = result
 		return result
