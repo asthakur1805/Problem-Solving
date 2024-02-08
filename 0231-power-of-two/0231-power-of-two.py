f07c2e5bfb5d9c45071732bba3b137494e036874
Aditya Thakur <ast.thakur@gmail.com>
2024-02-08 00:28:58 -0500

    Time: 30 ms (91.51%), Space: 16.5 MB (69.32%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -2,17 +2,6 @@ class Solution:
 
 	def isPowerOfTwo(self,inputNumber):
 
-		if inputNumber < 0:
-
-			return False
-
-		setBitCount = 0
-
-		for bitPosition in range(32):
-
-			setBitCount += (inputNumber & 1)
-			inputNumber >>= 1
-
-		return setBitCount == 1
+		return inputNumber > 0 and (inputNumber & (inputNumber-1)) == 0
 
 			
