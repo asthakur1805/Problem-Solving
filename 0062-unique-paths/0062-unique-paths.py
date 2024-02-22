73930bf5076f6eafb81ae2046aeb427d39f74689
Aditya Thakur <ast.thakur@gmail.com>
2024-02-22 11:05:52 -0500

    Time: 41 ms (29.06%), Space: 16.7 MB (56.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,23 +2,27 @@ class Solution:
 
 	def uniquePaths(self,numRows,numColumns):
 
-		dp = [[0]*numColumns for _ in range(numRows)]
+		prev = [0]*numColumns
 
 		for currRow in range(numRows-1,-1,-1):
+
+			dp = [0]*numColumns
 
 			for currColumn in range(numColumns-1,-1,-1):
 
 				if (currRow,currColumn) == (numRows-1,numColumns-1):
 
-					dp[currRow][currColumn] = 1
+					dp[currColumn] = 1
 
 				else:
 
-					down = dp[currRow+1][currColumn] if currRow+1<numRows else 0
-					right = dp[currRow][currColumn+1] if currColumn+1<numColumns else 0
+					down = prev[currColumn] if currRow+1<numRows else 0
+					right = dp[currColumn+1] if currColumn+1<numColumns else 0
 
-					dp[currRow][currColumn] = down+right
+					dp[currColumn] = down+right
 
-		return dp[0][0]
+			prev = dp
+
+		return prev[0]
 
 		
