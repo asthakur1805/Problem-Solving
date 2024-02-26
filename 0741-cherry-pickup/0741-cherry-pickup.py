222d3fd271f17a5c9999f0f6f551d086bdfbd47c
Aditya Thakur <ast.thakur@gmail.com>
2024-02-26 14:40:52 -0500

    Time: 584 ms (24.59%), Space: 19.6 MB (80.00%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.py
+++ 0741-cherry-pickup/0741-cherry-pickup.py
@@ -1,37 +1,38 @@
 class Solution:
 
 	def cherryPickup(self,grid):
+	
+		order = len(grid)
+		
+		dp = [[[float('-inf')]*order for _ in range(order)] for _ in range(order)]
 
-		return max(0,self.helper(grid,0,0,0,len(grid),{}))
+		for firstRow in range(order-1,-1,-1):
 
-	def helper(self,grid,firstRow,firstColumn,secondRow,order,cache):
+			for firstColumn in range(order-1,-1,-1):
 
-		secondColumn = firstRow+firstColumn-secondRow
+				for secondRow in range(order-1,-1,-1):
 
-		if firstRow >= order or secondRow >= order or firstColumn >= order or secondColumn >= order or grid[firstRow][firstColumn] == -1 or grid[secondRow][secondColumn] == -1:
+					secondColumn = firstRow+firstColumn-secondRow
+		
+					if 0 <= secondColumn < order and grid[firstRow][firstColumn] != -1 and grid[secondRow][secondColumn] != -1:
 
-			return float('-inf')
+						dp[firstRow][firstColumn][secondRow] = grid[firstRow][firstColumn]
 
-		if (firstRow,firstColumn,secondColumn) in cache:
+						if (firstRow,firstColumn) != (secondRow,secondColumn):
 
-			return cache[(firstRow,firstColumn,secondColumn)]
+							dp[firstRow][firstColumn][secondRow] += grid[secondRow][secondColumn]
 
-		result = grid[firstRow][firstColumn]
+						if (firstRow,firstColumn) != (order-1,order-1):
 
-		if (firstRow,firstColumn) != (secondRow,secondColumn):
+							bothRight = dp[firstRow][firstColumn+1][secondRow] if firstColumn+1<order else float('-inf')
+							bothDown = dp[firstRow+1][firstColumn][secondRow+1] if firstRow+1<order and secondRow+1<order else float('-inf')
+							downRight = dp[firstRow+1][firstColumn][secondRow] if firstRow+1<order else float('-inf')
+							rightDown = dp[firstRow][firstColumn+1][secondRow+1] if firstColumn+1<order and secondRow+1<order else float('-inf')
 
-			result += grid[secondRow][secondColumn]
+							dp[firstRow][firstColumn][secondRow] += max(bothRight,bothDown,downRight,rightDown)
 
-		if (firstRow,firstColumn) != (order-1,order-1):
+		return max(0,dp[0][0][0])
 
-			bothRight = self.helper(grid,firstRow,firstColumn+1,secondRow,order,cache)
-			bothDown = self.helper(grid,firstRow+1,firstColumn,secondRow+1,order,cache)
-			downRight = self.helper(grid,firstRow+1,firstColumn,secondRow,order,cache)
-			rightDown = self.helper(grid,firstRow,firstColumn+1,secondRow+1,order,cache)
-
-			result += max(bothRight,bothDown,downRight,rightDown)
-
-		cache[(firstRow,firstColumn,secondColumn)] = result
-		return result
+						
 
 			
