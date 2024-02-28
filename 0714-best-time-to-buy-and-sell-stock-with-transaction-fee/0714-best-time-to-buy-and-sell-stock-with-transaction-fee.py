0e6cc3cd4d41cd567196234c4119ceeaf8cf950a
Aditya Thakur <ast.thakur@gmail.com>
2024-02-28 15:07:52 -0500

    Time: 613 ms (41.08%), Space: 23.5 MB (97.83%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,24 +2,28 @@ class Solution:
 
 	def maxProfit(self,prices,fee):
 
-		dp = [[0]*2 for _ in range(len(prices)+1)]
+		prev = [0]*2
 
 		for index in range(len(prices)-1,-1,-1):
+
+			dp = [0]*2
 
 			for canBuy in range(2):
 
 				if canBuy:
 
-					buy = -prices[index]+dp[index+1][0]
-					notBuy = dp[index+1][1]
+					buy = -prices[index]+prev[0]
+					notBuy = prev[1]
 
-					dp[index][canBuy] = max(buy,notBuy)
+					dp[canBuy] = max(buy,notBuy)
 
 				else:
 
-					sell = prices[index]-fee+dp[index+1][1]
-					notSell = dp[index+1][0]
+					sell = prices[index]-fee+prev[1]
+					notSell = prev[0]
 
-					dp[index][canBuy] = max(sell,notSell)
+					dp[canBuy] = max(sell,notSell)
 
-		return dp[0][1]
+			prev = dp
+
+		return prev[1]
