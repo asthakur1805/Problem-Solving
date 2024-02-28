08ce0169083d8cc4efd42e81b1bd605280847070
Aditya Thakur <ast.thakur@gmail.com>
2024-02-28 13:09:12 -0500

    Time: 118 ms (56.68%), Space: 16.6 MB (94.40%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -2,9 +2,11 @@ class Solution:
 
 	def maxProfit(self,k,prices):
 
-		dp = [[[0]*(k+1) for _ in range(2)] for _ in range(len(prices)+1)]
+		prev = [[0]*(k+1) for _ in range(2)]
 
 		for index in range(len(prices)-1,-1,-1):
+
+			dp = [[0]*(k+1) for _ in range(2)]
 
 			for canBuy in range(2):
 
@@ -12,16 +14,18 @@ class Solution:
 
 					if canBuy:
 
-						buy = -prices[index]+dp[index+1][0][maxTransactions]
-						notBuy = dp[index+1][1][maxTransactions]
+						buy = -prices[index]+prev[0][maxTransactions]
+						notBuy = prev[1][maxTransactions]
 
-						dp[index][canBuy][maxTransactions] = max(buy,notBuy)
+						dp[canBuy][maxTransactions] = max(buy,notBuy)
 
 					else:
 
-						sell = prices[index]+dp[index+1][1][maxTransactions-1]
-						notSell = dp[index+1][0][maxTransactions]
+						sell = prices[index]+prev[1][maxTransactions-1]
+						notSell = prev[0][maxTransactions]
 
-						dp[index][canBuy][maxTransactions] = max(sell,notSell)
+						dp[canBuy][maxTransactions] = max(sell,notSell)
 
-		return dp[0][1][k]
+			prev = dp
+
+		return prev[1][k]
