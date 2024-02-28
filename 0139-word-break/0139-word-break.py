1593923547f25ad4abae9f4e08c59926a8166db6
Aditya Thakur <ast.thakur@gmail.com>
2024-02-27 20:48:29 -0500

    Time: 43 ms (45.26%), Space: 16.6 MB (77.83%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,53 +1,27 @@
-class TrieNode:
-
-	def __init__(self):
-
-		self.children = {}
-		self.endOfWord = False
-		
 class Solution:
 
 	def wordBreak(self,inputStr,wordDict):
 
-		root = TrieNode()
+		wordSet = set(wordDict)
 
-		for word in wordDict:
+		return self.helper(inputStr,0,wordSet,{})
 
-			curr = root
+	def helper(self,inputStr,start,wordSet,cache):
 
-			for char in word:
+		if start == len(inputStr):
 
-				if char not in curr.children:
-
-					curr.children[char] = TrieNode()
-
-				curr = curr.children[char]
-
-			curr.endOfWord = True
-
-		dp = [False]*(len(inputStr)+1)
-
-		dp[len(inputStr)] = True
-
-		for start in range(len(inputStr)-1,-1,-1):
-
-			curr = root
-
-			for end in range(start,len(inputStr)):
-
-				char = inputStr[end]
-
-				if char not in curr.children:
-
-					break
-
-				curr = curr.children[char]
-
-				if curr.endOfWord and dp[end+1]:
-
-					dp[start] = True
-					break
-
-		return dp[0]
-
+			return True
 		
+		if start in cache:
+
+			return cache[start]
+
+		for end in range(start,len(inputStr)):
+
+			if inputStr[start:end+1] in wordSet and self.helper(inputStr,end+1,wordSet,cache):
+
+				cache[start] = True
+				return True
+
+		cache[start] = False
+		return False
