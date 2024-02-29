f98774b2c5eddb040847d0189eec549b8a84ca3b
Aditya Thakur <ast.thakur@gmail.com>
2024-02-29 10:37:35 -0500

    Time: 8413 ms (5.00%), Space: 17.5 MB (69.67%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,27 @@
+class Solution:
+
+	def lengthOfLIS(self,nums):
+
+		return self.helper(nums,0,-1,{})
+
+	def helper(self,nums,currIndex,prevPickedIndex,cache):
+
+		if currIndex == len(nums):
+
+			return 0
+
+		if prevPickedIndex in cache:
+
+			return cache[prevPickedIndex]
+
+		if prevPickedIndex in cache:
+
+			return cache[prevPickedIndex]
+
+		pick = (1 + self.helper(nums,currIndex+1,currIndex,cache)) if prevPickedIndex == -1 or nums[currIndex] > nums[prevPickedIndex] else float('-inf')
+		notPick = self.helper(nums,currIndex+1,prevPickedIndex,cache)
+
+		cache[prevPickedIndex] = max(pick,notPick)
+		return cache[prevPickedIndex] 
+
+	
