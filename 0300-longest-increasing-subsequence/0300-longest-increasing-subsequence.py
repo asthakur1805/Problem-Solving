59bf635ff8076a7d2b021d98bcdea75787ae4e37
Aditya Thakur <ast.thakur@gmail.com>
2024-03-01 12:23:36 -0500

    Time: 4202 ms (5.00%), Space: 16.9 MB (95.64%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,22 +2,15 @@ class Solution:
 
 	def lengthOfLIS(self,nums):
 
-		next = [0]*(len(nums)+1)
+		dp = [1]*len(nums)
 
-		for currIndex in range(len(nums)-1,-1,-1):
+		for currIndex in range(1,len(nums)):
 
-			dp = [0]*(len(nums)+1)
+			for prevIndex in range(currIndex):
 
-			for prevIndex in range(currIndex-1,-2,-1):
+				pick = 1 + dp[prevIndex] if nums[prevIndex] < nums[currIndex] else float('-inf')
+				notPick = dp[currIndex]
 
-					pick = 1 + next[currIndex+1] if prevIndex == -1 or nums[currIndex] > nums[prevIndex] else float('-inf')
-					notPick = next[prevIndex+1]			
-
-					dp[prevIndex+1] = max(pick,notPick)
-
-			next = dp
-
-		return next[0]
-
+				dp[currIndex] = max(pick,notPick)
 		
-		
+		return max(dp)
