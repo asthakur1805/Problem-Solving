70d6a0590dfc9a751d39043da7ba531bc5a43abf
Aditya Thakur <ast.thakur@gmail.com>
2024-03-09 14:08:39 -0500

    Time: 9077 ms (7.67%), Space: 16.6 MB (80.79%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,35 +2,26 @@ class Solution:
 
 	def isMatch(self,inputStr,pattern):
 
-		return self.helper(inputStr,pattern,0,0,{})
+		return self.helper(inputStr,pattern,0,0)
 
-	def helper(self,inputStr,pattern,strIndex,patIndex,cache):
+	def helper(self,inputStr,pattern,strIndex,patIndex):
 
-		if strIndex >= len(inputStr) and patIndex >= len(pattern):
+		if strIndex == len(inputStr) and patIndex == len(pattern):
 
 			return True
 
-		if patIndex >= len(pattern):
+		if patIndex == len(pattern):
 
 			return False
 
-		if (strIndex,patIndex) in cache:
-
-			return cache[(strIndex,patIndex)]
-
 		match = strIndex < len(inputStr) and (inputStr[strIndex] == pattern[patIndex] or pattern[patIndex] == '.')
 
-		if patIndex+1 < len(pattern) and pattern[patIndex+1] == '*':
+		if patIndex+1<len(pattern) and pattern[patIndex+1] == '*':
 
-			cache[(strIndex,patIndex)] = self.helper(inputStr,pattern,strIndex,patIndex+2,cache) or match and self.helper(inputStr,pattern,strIndex+1,patIndex,cache)
-			return cache[(strIndex,patIndex)]
+			return self.helper(inputStr,pattern,strIndex,patIndex+2) or match and self.helper(inputStr,pattern,strIndex+1,patIndex)
 
 		if match:
 
-			cache[(strIndex,patIndex)] = self.helper(inputStr,pattern,strIndex+1,patIndex+1,cache)
-			return cache[(strIndex,patIndex)]
+			return self.helper(inputStr,pattern,strIndex+1,patIndex+1)
 
-		cache[(strIndex,patIndex)] = False
 		return False
-
-		
