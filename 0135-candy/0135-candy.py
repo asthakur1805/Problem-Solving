a01febc197fee6358e43b1cb7ec57875dd16c378
Aditya Thakur <ast.thakur@gmail.com>
2024-03-10 00:42:29 -0500

    Time: 121 ms (77.13%), Space: 19.2 MB (92.01%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -2,22 +2,30 @@ class Solution:
 
 	def candy(self,ratings):
 
-		candies = [1]*len(ratings)
+		index, totalCandies = 1, len(ratings)
 
-		for index in range(1,len(ratings)):
+		while index < len(ratings):
 
-			if ratings[index] > ratings[index-1]:
+			peak, dip = 0, 0
 
-				candies[index] = candies[index-1]+1
+			if ratings[index] == ratings[index-1]:
 
-		totalCandies = candies[len(ratings)-1]
+				index += 1
+				continue
 
-		for index in range(len(ratings)-2,-1,-1):
+			while index < len(ratings) and ratings[index] > ratings[index-1]:
 
-			if ratings[index] > ratings[index+1]:
+				peak += 1
+				totalCandies += peak
+				index += 1
 
-				candies[index] = max(candies[index],candies[index+1]+1)
-				
-			totalCandies += candies[index]
+			while index < len(ratings) and ratings[index] < ratings[index-1]:
+
+				dip += 1
+				totalCandies += dip
+				index += 1
+
+			totalCandies -= min(peak,dip)
 
 		return totalCandies
+	
