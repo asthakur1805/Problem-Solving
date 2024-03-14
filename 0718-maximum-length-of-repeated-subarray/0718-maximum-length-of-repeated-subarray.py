d24d41eb97abdbe348116a65e70baaab609affd9
Aditya Thakur <ast.thakur@gmail.com>
2024-03-14 00:54:05 -0400

    Time: 1362 ms (54.17%), Space: 16.7 MB (87.24%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -2,17 +2,21 @@ class Solution:
 
 	def findLength(self,firstArr,secondArr):
 
-		dp = [[0]*(len(secondArr)+1) for _ in range(len(firstArr)+1)]
+		prev = [0]*(len(secondArr)+1)
 
 		result = 0
 
 		for firstIndex in range(1,len(firstArr)+1):
 
+			dp = [0]*(len(secondArr)+1)
+
 			for secondIndex in range(1,len(secondArr)+1):
 
 				if firstArr[firstIndex-1] == secondArr[secondIndex-1]:
 
-					dp[firstIndex][secondIndex] = 1 + dp[firstIndex-1][secondIndex-1]
-					result = max(result,dp[firstIndex][secondIndex])
+					dp[secondIndex] = 1 + prev[secondIndex-1]
+					result = max(result,dp[secondIndex])
+
+			prev = dp
 
 		return result
