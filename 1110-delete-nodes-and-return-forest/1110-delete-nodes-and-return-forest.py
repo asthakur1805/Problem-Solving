1018ac2189fe0aa96e22a67ce7d3c0d1022743ef
Aditya Thakur <ast.thakur@gmail.com>
2024-03-13 23:30:27 -0400

    Time: 60 ms (33.38%), Space: 17 MB (89.81%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -0,0 +1,43 @@
+class Solution:
+
+	def delNodes(self,root,to_delete):
+
+		result = []
+
+		to_delete = set(to_delete)
+
+		self.helper(root,to_delete,result)
+
+		if root and root.val not in to_delete:
+
+			result.append(root)
+
+		return result
+
+	def helper(self,node,to_delete,result):
+
+		if not node:
+
+			return
+
+		node.left = self.helper(node.left,to_delete,result)
+
+		node.right = self.helper(node.right,to_delete,result)
+
+		if node.val in to_delete:
+
+			if node.left:
+
+				result.append(node.left)
+
+			if node.right:
+
+				result.append(node.right)
+
+			return
+
+		else:
+
+			return node
+
+		
