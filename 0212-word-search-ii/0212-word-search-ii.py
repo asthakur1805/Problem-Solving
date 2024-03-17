1d7d79deb4f84811ed5ab326ea71ec661e459f23
Aditya Thakur <ast.thakur@gmail.com>
2024-03-17 03:02:08 -0400

    Time: 7511 ms (13.69%), Space: 18.9 MB (46.02%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,63 @@
+class TrieNode:
+
+	def __init__(self):
+
+		self.children = {}
+		self.endOfWord = False
+
+class Solution:
+
+	def findWords(self,board,words):
+
+		root, visited, builder, directions = TrieNode(), set(), [], [(-1,0),(1,0),(0,-1),(0,1)]
+
+		numRows, numColumns = len(board), len(board[0])
+
+		result = set()
+
+		for word in words:
+
+			self.addWord(root,word)
+
+		for startRow in range(numRows):
+
+			for startColumn in range(numColumns):
+
+				self.dfs(board,startRow,startColumn,root,builder,numRows,numColumns,visited,directions,result)
+
+		return list(result)
+
+	def dfs(self,board,currRow,currColumn,curr,builder,numRows,numColumns,visited,directions,result):
+
+		if not(0 <= currRow < numRows) or not(0 <= currColumn < numColumns) or (currRow,currColumn) in visited or board[currRow][currColumn] not in curr.children:
+
+			return
+
+		builder.append(board[currRow][currColumn])
+		visited.add((currRow,currColumn))
+		curr = curr.children[board[currRow][currColumn]]
+
+		if curr.endOfWord:
+
+			result.add(''.join(builder))
+
+		for rowDirection,columnDirection in directions:
+
+			self.dfs(board,currRow+rowDirection,currColumn+columnDirection,curr,builder,numRows,numColumns,visited,directions,result)
+
+		visited.remove((currRow,currColumn))
+		builder.pop()
+
+	def addWord(self,root,word):
+
+		curr = root
+
+		for currChar in word:
+
+			if currChar not in curr.children:
+
+				curr.children[currChar] = TrieNode()
+
+			curr = curr.children[currChar]
+
+		curr.endOfWord = True
